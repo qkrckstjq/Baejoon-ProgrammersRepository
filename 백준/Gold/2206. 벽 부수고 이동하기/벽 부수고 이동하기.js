999e12c485530668eae66b92275932079b51e5c6
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2023-07-23 20:17:42 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 1676 ms, Memory: 292640 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.js
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.js
@@ -1,9 +1,73 @@
-let fs = require("fs");
-let input = fs.readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split("\n");
+// const input = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split("\n");
+
+// let [N, M] = input[0].split(' ');
+// N = +N - 1;
+// M = +M - 1;
+
+// const [map, visited] = input.reduce((acc, cur, idx) => {
+//   if(idx > 0) {
+//     let data = []
+//     for(let i = 0; i < cur.length; i++) {
+//       if(cur[i] !== '\r') {
+//         data.push(+cur[i]);
+//       }
+//     }
+//     acc[0].push(data);
+//     acc[1].push(new Array(M+1).fill(false));
+//   }
+//   return acc;
+// }, [[], []]);
+
+// const RY = [-1, 1, 0, 0];
+// const RX = [0, 0, -1, 1];
+// let result = 0;
+
+// //y,x,뿌신여부,진행상황카운트
+// const queue = [[0, 0, false,1]];
+// let index = 0;
+// while(queue.length - index !== 0) {
+//   let [Y,X,isBk,cnt] = queue[index];
+//   index++;
+//   result = cnt
+//   visited[Y][X] = true;
+//   if(Y === N && X === M) {
+//     console.log(result);
+//     break;
+//   }
+//   for(let i = 0; i < 4; i++) {
+//     let nextY = Y+RY[i];
+//     let nextX = X+RX[i];
+//     if(nextY >= 0 && nextY <= N && nextX >= 0 && nextX <= M) {
+//       console.log(`Y:${Y} X:${X} nextY:${nextY} nextX:${nextX} isB:${isBk}\n`)
+//       if(map[nextY][nextX] === 0 && !visited[nextY][nextX]){
+//         queue.push([nextY,nextX,isBk,cnt+1]);
+//       } else if (map[nextY][nextX] === 1 && !isBk && !visited[nextY][nextX]) {
+//         queue.push([nextY,nextX,true,cnt+1]);
+//       }
+//     }
+//   }
+//   if(queue.length-index === 0) {
+//     console.log(-1)
+//     break;
+//   }
+// }
+
+const input = require('fs').readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split("\n");
 
 let [N, M] = input[0].split(" ").map(Number);
-input = input.slice(1).map((v) => v.split("").map(Number));
-const ch = Array.from(new Array(N), () => new Array());
+const [map, ch] = input.reduce((acc, cur, idx) => {
+    if(idx > 0) {
+      let data = []
+      for(let i = 0; i < cur.length; i++) {
+        if(cur[i] !== '\r') {
+          data.push(+cur[i]);
+        }
+      }
+      acc[0].push(data);
+      acc[1].push(new Array(M+1).fill(false));
+    }
+    return acc;
+  }, [[], []]);
 const dx = [1, 0, -1, 0];
 const dy = [0, 1, 0, -1];
 const queue = [];
@@ -14,14 +78,18 @@ for (let i = 0; i < N; i++) {
   }
 }
 
-queue.push([0, 0, 0]);
+queue.push(0, 0, 0);
 ch[0][0][0] = 1;
 
 function BFS() {
   let idx = 0;
 
   while (idx !== queue.length) {
-    const [y, x, isBreak] = queue[idx];
+    const y= queue[idx];
+    idx++;
+    const x = queue[idx];
+    idx++;
+    const isBreak = queue[idx];
 
     if (x === M - 1 && y === N - 1) {
       return ch[y][x][isBreak];
@@ -31,12 +99,16 @@ function BFS() {
       const [nx, ny] = [x + dx[i], y + dy[i]];
 
       if (nx >= 0 && nx < M && ny >= 0 && ny < N) {
-        if (input[ny][nx] === 0 && ch[ny][nx][isBreak] === 0) {
+        if (map[ny][nx] === 0 && ch[ny][nx][isBreak] === 0) {
           ch[ny][nx][isBreak] = ch[y][x][isBreak] + 1;
-          queue.push([ny, nx, isBreak]);
-        } else if (input[ny][nx] === 1 && isBreak === 0) {
+          queue.push(ny);
+          queue.push(nx)
+          queue.push(isBreak)
+        } else if (map[ny][nx] === 1 && isBreak === 0) {
           ch[ny][nx][isBreak + 1] = ch[y][x][isBreak] + 1;
-          queue.push([ny, nx, isBreak + 1]);
+          queue.push(ny);
+          queue.push(nx)
+          queue.push(isBreak+1)
         }
       }
     }
