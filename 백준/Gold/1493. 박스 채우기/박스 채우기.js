ca00f2dbbd79677e8c03338d85c5e6a26200ce1d
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2023-07-25 16:28:29 +0900

    [Gold III] Title: 박스 채우기, Time: 500 ms, Memory: 224440 KB -BaekjoonHub

--- 백준/Gold/1493. 박스 채우기/박스 채우기.js
+++ 백준/Gold/1493. 박스 채우기/박스 채우기.js
@@ -1,24 +1,16 @@
-const fs = require("fs");
-const input = fs
-  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
-  .toString()
-  .trim()
-  .split("\n");
-
-const [len,wid,hei] = input[0].split(' ').map(el=>Number(el))
-const cube = input.slice(2).map(el=> el.split(' ').map((item,idx)=>idx===0 ? Number(2**item) : Number(item) ) );
-let answer = 0;
-let NoMore;
-let stack = [];
-
-stack.push(len,wid,hei);
-
-while(stack.length !== 0) {
-    let l=stack.pop(),w=stack.pop(),h=stack.pop();
+var input = require("fs").readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split("\n");
+var _a = input[0].split(' ').map(function (el) { return Number(el); }), len = _a[0], wid = _a[1], hei = _a[2];
+var cube = input.slice(2).map(function (el) { return el.split(' ').map(function (item, idx) { return idx === 0 ? Number(Math.pow(2, item)) : Number(item); }); });
+var answer = 0;
+var NoMore;
+var stack = [];
+stack.push(len, wid, hei);
+while (stack.length !== 0) {
+    var l = stack.pop(), w = stack.pop(), h = stack.pop();
     NoMore = true;
-    let Max = Math.min(l,w,h);
-    for(let i = cube.length-1; i >= 0; i--) {
-        if(cube[i][0] <= Max && cube[i][1] !== 0) {
+    var Max = Math.min(l, w, h);
+    for (var i = cube.length - 1; i >= 0; i--) {
+        if (cube[i][0] <= Max && cube[i][1] !== 0) {
             answer++;
             cube[i][1]--;
             Max = cube[i][0];
@@ -26,10 +18,16 @@ while(stack.length !== 0) {
             break;
         }
     }
-    if(NoMore) break;
-    if(w-Max !== 0) {stack.push(Max,w-Max,Max)}
-    if(l-Max !== 0) {stack.push(l-Max,w,Max)}
-    if(h-Max !== 0) {stack.push(l,w,h-Max)}
+    if (NoMore)
+        break;
+    if (w - Max !== 0) {
+        stack.push(Max, w - Max, Max);
+    }
+    if (l - Max !== 0) {
+        stack.push(l - Max, w, Max);
+    }
+    if (h - Max !== 0) {
+        stack.push(l, w, h - Max);
+    }
 }
-
-console.log(NoMore ? -1 : answer)
+console.log(NoMore ? -1 : answer);
