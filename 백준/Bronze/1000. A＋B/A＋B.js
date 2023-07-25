b7fc9091f705d755ffecbe3250059f4a66ae1e7a
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2023-07-25 16:41:26 +0900

    [Bronze V] Title: A+B, Time: 164 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -0,0 +1,3 @@
+const input = require("fs").readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt").toString().trim().split("\n");
+const [a,b] = input[0].split(' ')
+console.log(Number(a)+Number(b))
