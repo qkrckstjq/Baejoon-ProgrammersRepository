c273f9acd33b208346d1412c7dddfafbbb8ec747
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2023-10-24 17:37:27 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 11.59 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,15 +1,10 @@
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
-        for(int i = 1; i < food.length; i++) {
-            int max = (food[i] % 2 == 0) ? food[i]/2 : (food[i]-1)/2;
-            for(int j = 0; j < max; j++) {
-                answer+=Integer.toString(i);
+        String answer = "0";
+        for(int i = food.length-1; i >= 0; i--) {
+            for(int j = 0; j < food[i]/2; j++) {
+                answer= Integer.toString(i) + answer + Integer.toString(i);
             }
-        }
-        answer+='0';
-        for(int i = answer.length()-2; i >= 0; i--) {
-            answer+=answer.charAt(i);
         }
         return answer;
     }
