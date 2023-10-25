3d051830a160e210d88f43789788b8b6ae333a9d
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2023-10-25 21:23:04 +0900

    [level 1] Title: 콜라 문제, Time: 0.03 ms, Memory: 70.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        while(a <= n) {
+            answer += n / a * b;
+            n = (n % a) + (n / a) * b;
+        }
+        return answer;
+    }
+}
