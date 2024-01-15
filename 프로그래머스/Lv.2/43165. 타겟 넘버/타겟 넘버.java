f0bc1a1d5f16192ddc6e469cf493474fab0c63d4
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-01-15 18:34:56 +0900

    [level Lv.2] Title: 타겟 넘버, Time: 7.24 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+import java.util.HashMap;
+import java.util.Map;
+
+
+class Solution {
+    public int solution(int[] numbers, int target) {
+        int answer = 0;
+        Stack<Integer[]> stack = new Stack<>();
+        Integer[] tempPlus = {0, numbers[0]};
+        Integer[] tempMinus = {0, numbers[0]*-1};
+        stack.push(tempPlus);
+        stack.push(tempMinus);
+        
+        while(!stack.isEmpty()) {
+            Integer[] popped = stack.pop();
+            int index = popped[0];
+            int value = popped[1];
+            if(index == numbers.length-1) {
+                answer += value == target ? 1 : 0;
+            } else {
+                Integer[] temp1 = {index+1, value+numbers[index+1]};
+                Integer[] temp2 = {index+1, value-numbers[index+1]};
+                stack.push(temp1);
+                stack.push(temp2);
+            }
+        }
+        
+        return answer;                
+    }
+}
