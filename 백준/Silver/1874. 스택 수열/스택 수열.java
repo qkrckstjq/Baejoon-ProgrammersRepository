fc240bd362e6a648d3977e3ca8d4d001a7dd0b18
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-01-19 20:18:50 +0900

    [Silver II] Title: 스택 수열, Time: 972 ms, Memory: 183632 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,6 +1,17 @@
 import java.util.Scanner;
 import java.util.Stack;
 
+//public class JsonFileReaderTest {
+////    public static void main(String[] args) {
+////        String AbRoute = "C:\\Users\\qkrckstjq\\Desktop\\backend\\java-server-example\\src\\Data.json";
+////        String rlRoute = "..\\Data.json";
+////        JsonFileReader reader1 = new JsonFileReader(AbRoute);
+////        JsonFileReader reader2 = new JsonFileReader(rlRoute);
+////        reader1.readJsonFileToText();
+////        reader2.readJsonFileToText();
+////    }
+//}
+
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -9,28 +20,27 @@ public class Main {
         int n = Integer.parseInt(scanner.nextLine());
 
         int currentNumber = 1;
+        //가장 마지막에 스택에 푸쉬될 수 + 1;
 
         for (int i = 0; i < n; i++) {
             int target = Integer.parseInt(scanner.nextLine());
 
-            // 현재 숫자가 목표 숫자보다 작을 때까지 push
             while (currentNumber <= target) {
                 stack.push(currentNumber);
                 answer.append("+\n");
                 currentNumber++;
             }
 
-            // 스택의 맨 위가 목표 숫자와 같을 때 pop
             if (!stack.isEmpty() && stack.peek() == target) {
                 stack.pop();
                 answer.append("-\n");
             } else {
-                // 만들 수 없는 경우
                 System.out.println("NO");
                 return;
             }
         }
-
+        //이미 스택이 비어있으면 만들 수 없음
         System.out.println(answer);
     }
 }
+// 1. 스택의 마지막 값이 입력받은 값보다 크면 이미 스택에서 팝을 할 시점을 놓침
