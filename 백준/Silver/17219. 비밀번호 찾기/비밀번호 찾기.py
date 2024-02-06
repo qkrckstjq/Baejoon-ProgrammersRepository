7cc2b1587f9ef5f7bc6f9de60629b71d0b8d4f24
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-02-06 22:18:30 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 276 ms, Memory: 49208 KB -BaekjoonHub

--- 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.py
@@ -1,17 +1,18 @@
-NM = input().split(" ")
+import sys
+
+NM = sys.stdin.readline().split()
 site_number = int(NM[0])
 hope_find_pw = int(NM[1])
 
 site_password = {}
 
 for i in range(site_number):
-    s_w = input().split(" ")
+    s_w = sys.stdin.readline().split()
     site = s_w[0]
-    password = s_w[1]
+    password = s_w[1].strip()  # 줄 바꿈 문자 제거
 
     site_password[site] = password
 
 for i in range(hope_find_pw):
-    site = input()
+    site = sys.stdin.readline().strip()  # 줄 바꿈 문자 제거
     print(site_password[site])
-
