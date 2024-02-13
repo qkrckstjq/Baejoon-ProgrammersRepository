6bfa20c80baab5029eeed16b04556243b73d398a
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-02-13 16:32:06 +0900

    Time: 44 ms (55.22%), Space: 18.8 MB (12.26%) - LeetHub

--- LeetCode/Easy/0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ LeetCode/Easy/0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -5,28 +5,27 @@ class TreeNode:
         self.left = left
         self.right = right
 class Solution:
+    max_len = 0
+    memo = {}
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        if not root:
+        self.recursion_tree(root)
+        return self.max_len
+        
+    def recursion_tree(self, root):
+        if root is None:
             return 0
-
-        stack = [[root, False]]
-        d = 0
-        height_dict = {None: 0}
-
-        while stack:
-            node, visited = stack.pop()
-
-            if node:
-                if visited:
-                    left_height = height_dict[node.left]
-                    right_height = height_dict[node.right]
-                    d = max(d, left_height + right_height)
-                    height_dict[node] = max(left_height, right_height) + 1
-                else:
-                    stack.append([node, True])
-                    stack.append([node.right, False])
-                    stack.append([node.left, False])
-            else:
-                height_dict[node] = 0
-
-        return d
+        if root in self.memo:
+            return self.memo[root]
+            
+        left_len = self.recursion_tree(root.left)
+        right_len = self.recursion_tree(root.right)
+        # print("현재 루트", root)
+        # print("현재 left",left_len)
+        # print("현재 right",right_len)
+        # print("현재 max_len", self.max_len)
+        self.memo[root] = left_len + right_len + 1
+        self.max_len = max(left_len + right_len, self.max_len)
+        return max(left_len, right_len) + 1
+        
+            
+        
