1c9d4dab0c6e0249c70c5ec7e4d13157e8780baa
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-02-13 11:38:05 +0900

    Time: 39 ms (80.75%), Space: 17.7 MB (62.56%) - LeetHub

--- LeetCode/Easy/0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ LeetCode/Easy/0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -5,48 +5,28 @@ class TreeNode:
         self.left = left
         self.right = right
 class Solution:
-    height = {}
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        max_diameter = 0
-        stack = [root]
-        
-        while stack:
-            cur_node = stack.pop()
-            cur_node_diameter = self.get_diameter(cur_node)
-            max_diameter = max(cur_node_diameter, max_diameter)
-            
-            if cur_node.left is not None:
-                stack.append(cur_node.left)
-            if cur_node.right is not None:
-                stack.append(cur_node.right)
-        return max_diameter
-    
-    def get_diameter(self, root):
-        if root is None:
+        if not root:
             return 0
-        left_len = self.get_max_depth(root.left)
-        right_len = self.get_max_depth(root.right)
-        return left_len + right_len
-        
-    
-    def get_max_depth(self, node):
-        if node is None:
-            return 0
-        
-        if node in self.height:
-            return height[node]
-        
-        result = 0
-        diameter = 0
-        stack = [[node, diameter]]
+
+        stack = [(root, False)]
+        d = 0
+        height_dict = {None: 0}
+
         while stack:
-            cur = stack.pop()
-            cur_node = cur[0]
-            cur_diameter = cur[1]
-            diameter = max(diameter, cur_diameter)
-            if cur_node.left is not None:
-                stack.append([cur_node.left, cur_diameter + 1])
-            if cur_node.right is not None:
-                stack.append([cur_node.right, cur_diameter + 1])
-        self.height[node] = diameter
-        return diameter + 1
+            node, visited = stack.pop()
+
+            if node:
+                if visited:
+                    left_height = height_dict[node.left]
+                    right_height = height_dict[node.right]
+                    d = max(d, left_height + right_height)
+                    height_dict[node] = max(left_height, right_height) + 1
+                else:
+                    stack.append((node, True))
+                    stack.append((node.right, False))
+                    stack.append((node.left, False))
+            else:
+                height_dict[node] = 0
+
+        return d
