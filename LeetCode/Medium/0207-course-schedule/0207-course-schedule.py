0c8ee5587c11dcb91a9f99b74eb20b3a5f3cf1a0
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-02-14 00:26:35 +0900

    Time: 92 ms (58.17%), Space: 18.6 MB (64.41%) - LeetHub

--- LeetCode/Medium/0207-course-schedule/0207-course-schedule.py
+++ LeetCode/Medium/0207-course-schedule/0207-course-schedule.py
@@ -1,34 +1,28 @@
+from collections import defaultdict
+
 class Solution:
-    def dfs_cycle(self, graph, vertex, visited, visit):
-        if vertex in visit:
-            return True
-        
-        if vertex in visited:
-            return False
-        
-        visited.add(vertex)
-        visit.add(vertex)
-        
-        if vertex in graph:
-            for next_vertex in graph[vertex]:
-                if self.dfs_cycle(graph, next_vertex, visited, visit):
-                    return True
-        
-        visit.remove(vertex)
-        return False
-    
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        graph = {}
-        
-        for vertex, require_vertex in prerequisites:
-            if vertex in graph:
-                graph[vertex].append(require_vertex)    
-            else:
-                graph[vertex] = [require_vertex]
-        
+    def canFinish(self, numCourses, prerequisites) -> bool:
         visited = set()
-        
-        for vertex in graph.keys():
-            if self.dfs_cycle(graph, vertex, visited, set()):
+        memo = set()
+        require_graph = defaultdict(list)
+        for target, require in prerequisites:
+            require_graph[target].append(require)
+
+        for node in range(numCourses):
+            if node not in memo and self.has_cycle(node, require_graph, visited, memo):
                 return False
         return True
+
+    def has_cycle(self, start_node, require_graph, visited, memo):
+        if start_node in memo:
+            return False
+        if start_node in visited:
+            return True
+        
+        visited.add(start_node)
+        for require in require_graph[start_node]:
+            if self.has_cycle(require, require_graph, visited, memo):
+                return True
+        visited.remove(start_node)
+        memo.add(start_node)
+        return False
