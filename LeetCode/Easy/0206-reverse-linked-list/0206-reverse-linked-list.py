34b1fb81af2338b8a490aa8f4cdba30e72bddece
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-02-13 17:06:55 +0900

    Time: 36 ms (73.10%), Space: 17.7 MB (90.68%) - LeetHub

--- LeetCode/Easy/0206-reverse-linked-list/0206-reverse-linked-list.py
+++ LeetCode/Easy/0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -12,55 +12,9 @@ class Solution:
         current_node = head
         
         while current_node is not None:
-            prev_next_node = current_node.next
-            current_node.next = reverse_node
-            reverse_node = current_node
+            prev_next_node = current_node.next  #현재 노드의 다음 노드 살려두기
+            current_node.next = reverse_node    #현재 노드의 다음을 끊어진 이전 노드로
+            reverse_node = current_node         #
             current_node = prev_next_node
         
         return reverse_node
-            
-            
-            
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
-# class Solution:
-#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-#         if head == None or head.next == None:
-#             return head
-
-#         prev_node = None
-#         cur_node = head
-#         while cur_node != None:
-#             next_node = cur_node.next
-#             cur_node.next = prev_node
-#             prev_node = cur_node
-#             cur_node = next_node
-
-#         return prev_node
