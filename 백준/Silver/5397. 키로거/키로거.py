2e60f5f2f3cadc5e6cdd7ac4dcfb1fcef6eca163
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-02-13 17:36:43 +0900

    [Silver II] Title: 키로거, Time: 1204 ms, Memory: 43640 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.py
+++ 백준/Silver/5397. 키로거/키로거.py
@@ -6,27 +6,27 @@ def sInput():
 T = int(sInput())
 
 for i in range(T):
-    key_log = sInput()
+    input_words = sInput()
     password = deque()
-    temp_key = deque()
+    shifted_word = deque()
 
-    for c in key_log:
+    for c in input_words:
         if c == '<':
             if len(password) != 0:
-                temp_key.appendleft(password.pop())
+                shifted_word.appendleft(password.pop())
             continue
         if c == '-':
             if len(password) != 0:
                 password.pop()
             continue
         if c == '>':
-            if len(temp_key) != 0:
-                password.append(temp_key.popleft())
+            if len(shifted_word) != 0:
+                password.append(shifted_word.popleft())
             continue
 
         password.append(c)
 
-    while len(temp_key) != 0:
-        password.append(temp_key.popleft())
+    while len(shifted_word) != 0:
+        password.append(shifted_word.popleft())
 
     print("".join(password))
