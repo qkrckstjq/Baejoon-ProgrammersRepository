24712b35c5f5549864425e01d1577c50ec21634c
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-02-13 12:14:34 +0900

    Time: 862 ms (27.60%), Space: 62.9 MB (94.88%) - LeetHub

---
+++ LeetCode/Medium/0077-combinations/0077-combinations.py
@@ -0,0 +1,22 @@
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        if n < 2:
+            return [[1]]
+        
+        stack = []
+        result = []
+        for num in range(n- (k - 1)):
+            stack.append([num+1])
+        
+        while len(stack) != 0:
+            cur_nums = stack.pop()
+            last_num = cur_nums[len(cur_nums)-1]
+            
+            if len(cur_nums) == k:
+                result.append(cur_nums)
+            
+            for num in range(last_num, n):
+                temp_nums = list(cur_nums)
+                temp_nums.append(num+1)
+                stack.append(temp_nums)
+        return result
