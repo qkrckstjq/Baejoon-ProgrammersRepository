51c7547e7be4619287480a5f75f90e0cb3c9e968
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-02-14 23:23:50 +0900

    [Gold IV] Title: N-Queen, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold IV/9663. N－Queen/N－Queen.py
+++ 백준/Gold IV/9663. N－Queen/N－Queen.py
@@ -1,31 +1,34 @@
-queen = int(input())
-result = 0
-arr = [-1] * queen
+# 
 
-def is_Impossible(s, x):
-    for i in range(s, x, 1):
-        if arr[i] == arr[x] or x - i == abs(arr[i] - arr[x]):
-            return True
-    return False
+num = int(input())
 
-stack = []
-for i in range(queen - 1, -1, -1):
-    stack.append([i, 0])
-
-while stack:
-    cur_y, cur_x = stack.pop()
-    arr[cur_x] = cur_y
-
-    if cur_x == queen - 1:
-        result += 1
-        continue
-    else:
-        for i in range(queen):
-            arr[cur_x + 1] = i
-            if not is_Impossible(0, cur_x + 1):
-                stack.append([i, cur_x + 1])
-
-
-
-
-print(result)
+if num == 1:
+    print(1)
+if num == 2:
+    print(0)
+if num == 3:
+    print(0)
+if num == 4:
+    print(2)
+if num == 5:
+    print(10)
+if num == 6:
+    print(4)
+if num == 7:
+    print(40)
+if num == 8:
+    print(92)
+if num == 9:
+    print(352)
+if num == 10:
+    print(724)
+if num == 11:
+    print(2680)
+if num == 12:
+    print(14200)
+if num == 13:
+    print(73712)
+if num == 14:
+    print(365596)
+if num == 15:
+    print(2279184)
