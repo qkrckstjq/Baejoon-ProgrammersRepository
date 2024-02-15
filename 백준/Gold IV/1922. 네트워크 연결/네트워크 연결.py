3701e69ece2cfc0bc12cc0df48b4ea1b6e80167b
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-02-15 17:15:13 +0900

    [Gold IV] Title: 네트워크 연결, Time: 712 ms, Memory: 64800 KB -BaekjoonHub

---
+++ 백준/Gold IV/1922. 네트워크 연결/네트워크 연결.py
@@ -0,0 +1,32 @@
+import sys, heapq
+from collections import defaultdict
+
+
+def s_input():
+    return sys.stdin.readline().strip()
+
+
+vertex = int(s_input())
+edge = int(s_input())
+graph = defaultdict(dict)
+min_heap = []
+visit = set()
+for _ in range(edge):
+    src, dst, w = list(map(int, s_input().split(" ")))
+    graph[src][dst] = w
+    graph[dst][src] = w
+
+result = 0
+heapq.heappush(min_heap, (0, 1))
+
+while min_heap:
+    cur_w, cur_node = heapq.heappop(min_heap)
+    if cur_node in visit:
+        continue
+    visit.add(cur_node)
+    result += cur_w
+    for next_node in list(graph[cur_node].keys()):
+        heapq.heappush(min_heap, (graph[cur_node][next_node], next_node))
+
+print(result)
+
