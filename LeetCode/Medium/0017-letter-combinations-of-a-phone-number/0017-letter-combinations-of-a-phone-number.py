3efa75ac16b4e9e470f166d4be5718e9c8750649
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-02-15 16:35:05 +0900

    Time: 42 ms (24.04%), Space: 16.4 MB (90.22%) - LeetHub

---
+++ LeetCode/Medium/0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,38 @@
+base = {
+    "2" : ["a","b","c"],
+    "3" : ["d","e","f"],
+    "4" : ["g","h","i"],
+    "5" : ["j","k","l"],
+    "6" : ["m","n","o"],
+    "7" : ["p","q","r","s"],
+    "8" : ["t","u","v"],
+    "9" : ["w","x","y","z"],
+}
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) < 1:
+            return []
+        
+        input_digit = list(digits)
+        stack = []
+        result = []
+        
+        for start_c in base[input_digit[0]]:
+            stack.append(start_c[0])
+        
+        while len(stack) != 0:
+            cur_str = stack.pop()
+            
+            if len(cur_str) == len(input_digit):
+                result.append(cur_str)
+            else:
+                for cur_c in base[input_digit[len(cur_str)]]:
+                    next_str = cur_str
+                    next_str += cur_c
+                    stack.append(next_str)
+                
+        return result
+                
+            
+        
+        
