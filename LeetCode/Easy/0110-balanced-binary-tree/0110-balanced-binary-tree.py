f989b68a28a64ee68c64b5e42a30e2b19910b7a5
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-02-16 23:56:06 +0900

    Time: 44 ms (72.84%), Space: 17.6 MB (79.18%) - LeetHub

--- LeetCode/Easy/0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ LeetCode/Easy/0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -1,21 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def helper(self, root):
-        if root is None:
-            return 0
-        leftValue = self.helper(root.left)
-        rightValue = self.helper(root.right)
-
-        # if parents is unbalanced, even children will be unbalanced
-        if leftValue == -1 or rightValue == -1:
-            return -1
-
-        if abs(leftValue - rightValue ) > 1:
-            return -1
-
-        return 1 + max(leftValue, rightValue)
-
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
-        answer = self.helper(root)
-        if answer != -1:
-            return True
-        return False
+        return True if self.get_height(root) != -1 else False
+    
+    def get_height(self, node):
+        if node is None:
+            return 0
+        left = self.get_height(node.left)
+        right = self.get_height(node.right)
+        
+        if abs(left - right) > 1:
+            return -1
+        if left == -1 or right == -1:
+            return -1
+        return max(left, right) + 1
