650392f57143f6c60353bc4579b2ebc4b6b40c5e
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-02-17 18:23:24 +0900

    Time: 36 ms (90.99%), Space: 17.5 MB (99.71%) - LeetHub

--- LeetCode/Easy/0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ LeetCode/Easy/0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -1,25 +1,12 @@
-# # Definition for a binary tree node.
-# # class TreeNode:
-# #     def __init__(self, val=0, left=None, right=None):
-# #         self.val = val
-# #         self.left = left
-# #         self.right = right
-# class Solution:
-#     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-#         max_len = {'max' : 0}
-#         self.find_max_len(root, max_len)
-#         return max_len['max']
-    
-#     def find_max_len(self, node, max_len):
-#         if node is None:
-#             return 0
-#         left_len = self.find_max_len(node.left, max_len)
-#         right_len = self.find_max_len(node.right, max_len)
-#         max_len['max'] = max(max_len['max'], left_len + right_len)
-#         return max(self.find_max_len(node.left, max_len), self.find_max_len(node.right, max_len)) + 1
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        max_len = {'max': 0}
+        max_len = {'max' : 0}
         self.find_max_len(root, max_len)
         return max_len['max']
     
