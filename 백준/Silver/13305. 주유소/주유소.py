ba7eab8b726abc48b8420b0d856bac7486bf22b2
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-02-20 14:49:12 +0900

    [Silver III] Title: 주유소, Time: 152 ms, Memory: 127828 KB, Score: 58 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,23 +1,26 @@
 import sys
 
+
 def s_input():
     return sys.stdin.readline().strip()
+
 
 N = int(s_input())
 city = list(map(int, s_input().split(" ")))
 total = sum(city)
 price = list(map(int, s_input().split(" ")))
-price.sort()
 result = 0
 for i in range(len(price)):
     price[i] = (price[i], i)
-    
+
+price.sort(key=lambda x:x[0])
+
 for i in range(len(price)):
-    if price[i][1] < len(city) - 1:
+    if price[i][1] <= len(city) - 1:
         temp = sum(city[price[i][1]:])
-        result = temp * price[i][0]
+        city = city[:price[i][1]]
+        result += temp * price[i][0]
         total -= temp
         if total == 0:
             break
 print(result)
-    
