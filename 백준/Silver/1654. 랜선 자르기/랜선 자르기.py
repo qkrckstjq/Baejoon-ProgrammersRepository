3dbb5978894041e95e8b3bf6b0faa8a15fc71ccd
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-02-21 22:26:12 +0900

    [Silver II] Title: 랜선 자르기, Time: 124 ms, Memory: 110432 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,32 +1,3 @@
-# import sys
-#
-# def s_input():
-#     return sys.stdin.readline().strip()
-#
-# def cut(arr, target):
-#     result = 0
-#     for value in arr:
-#         if value > target:
-#             result += (value - target)
-#     return result
-#
-# N, M = list(map(int, s_input().split(" ")))
-# trees = list(map(int, s_input().split(" ")))
-# min_v = 1
-# max_v = max(trees)
-#
-# while min_v <= max_v:
-#     mid_v = (min_v + max_v) // 2
-#     cut_v = cut(trees, mid_v)
-#     # if cut_v == M:
-#     #     print(mid_v)
-#     #     break
-#     if cut_v < M:
-#         max_v = mid_v - 1
-#     else:
-#         min_v = mid_v + 1
-# print(max_v)
-
 import sys
 
 def s_input():
