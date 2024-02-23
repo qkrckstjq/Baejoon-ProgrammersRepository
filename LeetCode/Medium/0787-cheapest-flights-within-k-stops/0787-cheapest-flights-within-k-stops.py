ed610efad0805504ebae8148becbcdf3c843d9e9
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-02-23 18:00:27 +0900

    Time: 111 ms (52.51%), Space: 18 MB (71.46%) - LeetHub

--- LeetCode/Medium/0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ LeetCode/Medium/0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,30 +1,24 @@
 import heapq
 from collections import defaultdict
 
+
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        max_d = 987654321
-        graph = [[] for _ in range(n + 1)]
-        dijkstra = [max_d] * (n + 1)
-        dijkstra[src] = 0
+        graph = defaultdict(dict)
         min_heap = []
-        visit = {}
-        for edge in flights:
-            u, v, w = edge
-            graph[u].append((v, w))
+        for u, v, w in flights:
+            graph[u][v] = w
             if u == src:
-                dijkstra[v] = w
                 heapq.heappush(min_heap, (w, v, 0))
-                
+        visit = {}
         while min_heap:
-            cur_w, cur_dst, cur_k = heapq.heappop(min_heap)
-            if cur_dst == dst:
+            cur_w, cur_src, cur_k = heapq.heappop(min_heap)
+            visit[cur_src] = cur_k
+            if dst == cur_src:
                 return cur_w
-            if cur_dst not in visit or cur_k < visit[cur_dst]:
-                visit[cur_dst] = cur_k
-                if cur_k < k:
-                    for next_dst, w in graph[cur_dst]:
-                        next_w = w + cur_w
-                        dijkstra[next_dst] = next_w
+            if cur_k < k:
+                for next_dst, w in graph[cur_src].items():
+                    next_w = w + cur_w
+                    if not next_dst in visit or cur_k + 1 < visit[next_dst]:
                         heapq.heappush(min_heap, (next_w, next_dst, cur_k + 1))
-        return - 1
+        return -1
