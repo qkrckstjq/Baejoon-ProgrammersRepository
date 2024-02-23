0139b064e8ad2a1485d51e737ca1f5291dddd8fa
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-02-23 21:39:55 +0900

    Time: 507 ms (85.91%), Space: 31 MB (65.01%) - LeetHub

--- LeetCode/Medium/0053-maximum-subarray/0053-maximum-subarray.py
+++ LeetCode/Medium/0053-maximum-subarray/0053-maximum-subarray.py
@@ -3,6 +3,7 @@ class Solution:
         max_num = nums[0]
         for i in range(1, len(nums)):
             nums[i] = nums[i] + (nums[i - 1] if nums[i - 1] > 0 else 0)
-            max_num = max(max_num, nums[i])
+            if max_num < nums[i]:
+                max_num = nums[i]
         return max_num
         
