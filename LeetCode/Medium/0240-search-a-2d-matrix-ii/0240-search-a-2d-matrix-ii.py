d2d62d0d1af056f93ee97d073230922db6c1ef22
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-02-24 18:00:35 +0900

    Time: 142 ms (53.88%), Space: 23.2 MB (49.06%) - LeetHub

--- LeetCode/Medium/0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ LeetCode/Medium/0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -1,33 +1,28 @@
 from collections import defaultdict
-
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        stack = [(len(matrix) // 2, len(matrix[0]) // 2)]
-        #stack = [(0, 0)]
+        mid_y = len(matrix) // 2
+        mid_x = len(matrix[0]) // 2
         visit = defaultdict(set)
-        while stack:
-            cur_y, cur_x = stack.pop()
-            cur_value = matrix[cur_y][cur_x]
-            visit[cur_y].add(cur_x)
-            if cur_value == target:
-                return True
-            if cur_value < target:
-                next_y = cur_y + 1
-                next_x = cur_x + 1
-                if not cur_x in visit[next_y] and self.is_possible(matrix, next_y, cur_x):
-                    stack.append((next_y, cur_x))
-                if not next_x in visit[cur_y] and self.is_possible(matrix, cur_y, next_x):
-                    stack.append((cur_y, next_x))
-            if cur_value > target:
-                next_y = cur_y - 1
-                next_x = cur_x - 1
-                if not cur_x in visit[next_y] and self.is_possible(matrix, next_y, cur_x):
-                    stack.append((next_y, cur_x))
-                if not next_x in visit[cur_y] and self.is_possible(matrix, cur_y, next_x):
-                    stack.append((cur_y, next_x))
-            
-        return False
-    
-    def is_possible(self, matrix, y, x):
-        return True if 0 <= y < len(matrix) and 0 <= x < len(matrix[0]) else False
-    
+        return self.recursion(matrix, mid_y, mid_x, target, visit)
+
+    def recursion(self, matrix, y, x, target, visit):
+        if self.out_of_range(matrix, y, x) or x in visit[y]:
+            return False
+        if matrix[y][x] == target:
+            return True
+        y_y = y
+        x_x = x
+        visit[y].add(x)
+        if matrix[y][x] < target:
+            y_y += 1
+            x_x += 1
+        else:
+            y_y -= 1
+            x_x -= 1
+        result_y = self.recursion(matrix, y_y, x, target, visit)
+        result_x = self.recursion(matrix, y, x_x, target, visit)
+        return True if result_y or result_x else False
+
+    def out_of_range(self, matrix, y, x):
+        return False if 0 <= y < len(matrix) and 0 <= x < len(matrix[0]) else True
