cc98d41bd71ab1fd0540baf9a3c0706fa5715902
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-02-26 17:08:40 +0900

    Time: 105 ms (46.15%), Space: 18 MB (47.20%) - LeetHub

--- LeetCode/Medium/0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ LeetCode/Medium/0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,24 +1,33 @@
+from typing import List
 import heapq
-from collections import defaultdict
 
+INF = float('inf')
 
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        graph = defaultdict(dict)
-        min_heap = []
+        graph = [[] for _ in range(n)]
         for u, v, w in flights:
-            graph[u][v] = w
-            if u == src:
-                heapq.heappush(min_heap, (w, v, 0))
-        visit = {}
-        while min_heap:
-            cur_w, cur_src, cur_k = heapq.heappop(min_heap)
-            visit[cur_src] = cur_k
-            if dst == cur_src:
-                return cur_w
-            if cur_k < k:
-                for next_dst, w in graph[cur_src].items():
-                    next_w = w + cur_w
-                    if not next_dst in visit or cur_k + 1 < visit[next_dst]:
-                        heapq.heappush(min_heap, (next_w, next_dst, cur_k + 1))
+            graph[u].append((v, w))
+        
+        pq = [(0, src, 0)]
+        
+        dist = [[INF for _ in range(k+2)] for _ in range(n)]
+        dist[src][0] = 0
+        
+        while pq:
+            cost, node, stops = heapq.heappop(pq)
+            
+            if node == dst:
+                return cost
+       
+            if stops > k:
+                continue
+            
+            for next_node, w in graph[node]:
+                next_cost = cost + w
+
+                if next_cost < dist[next_node][stops + 1]:
+                    dist[next_node][stops + 1] = next_cost
+                    heapq.heappush(pq, (next_cost, next_node, stops + 1))
+
         return -1
