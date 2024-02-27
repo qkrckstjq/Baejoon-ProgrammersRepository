8d87297d375a658c13392ff02515a697a736d3e9
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-02-27 20:16:11 +0900

    Time: 39 ms (29.58%), Space: 16.4 MB (93.83%) - LeetHub

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.py
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,7 +1,22 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo = [0, 1, 2]
-        for i in range(3, n + 1):
-            memo.append(memo[i - 1] + memo[i - 2])
-        return memo[n]
-        
+        def factorial(n):
+            return_value = 1
+            for i in range(1, n + 1, 1):
+                return_value *= i
+
+            return return_value
+
+        def combination(n, r):
+            mid_v = factorial(n) / factorial(n - r)
+            return mid_v / factorial(r)
+
+        a = n
+        b = 0
+        total = 0
+        while a >= b:
+            total += combination(a, b)
+            a -= 1
+            b += 1
+
+        return int(total)
