162a3d786bb6744fdd1a869b332244617d5c0b66
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-02-27 20:20:07 +0900

    Time: 491 ms (93.62%), Space: 31 MB (61.31%) - LeetHub

--- LeetCode/Medium/0053-maximum-subarray/0053-maximum-subarray.py
+++ LeetCode/Medium/0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,9 +1,34 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        max_num = nums[0]
-        for i in range(1, len(nums)):
-            nums[i] = nums[i] + (nums[i - 1] if nums[i - 1] > 0 else 0)
-            if max_num < nums[i]:
-                max_num = nums[i]
-        return max_num
-        
+        def cut_first(nums):
+            for i in range(0, len(nums), 1):
+                if nums[i] > 0:
+                    return i
+            return -1
+
+        def cut_last(nums):
+            for i in range(len(nums) - 1, -1, -1):
+                if nums[i] > 0:
+                    return i
+
+            return -1
+
+        def get_max_subarray(front_i, last_i):
+            if front_i == -1 and last_i == -1:
+                return max(nums)
+
+            total = 0
+            sum_total = []
+            for num in nums[front_i:last_i + 1]:
+                if total + num > 0:
+                    total += num
+                    sum_total.append(total)
+
+                else:
+                    total = 0
+
+            return max(sum_total)
+
+        front_i = cut_first(nums)
+        last_i = cut_last(nums)
+        return get_max_subarray(front_i, last_i)
