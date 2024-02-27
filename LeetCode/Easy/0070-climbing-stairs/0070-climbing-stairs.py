29d13d2466e6eb3716af3258e61bc9675652ede3
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-02-27 19:29:15 +0900

    Time: 36 ms (51.23%), Space: 16.5 MB (72.87%) - LeetHub

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.py
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,8 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo = [0]
-        memo.append(1)
-        memo.append(2)
+        memo = [0, 1, 2]
         for i in range(3, n + 1):
             memo.append(memo[i - 1] + memo[i - 2])
         return memo[n]
