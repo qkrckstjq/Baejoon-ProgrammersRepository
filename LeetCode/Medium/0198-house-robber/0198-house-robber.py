ee4c9b16eaa3b88e30948c0c5dcacdc6a3767737
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-02-27 19:30:46 +0900

    Time: 27 ms (96.30%), Space: 16.5 MB (57.70%) - LeetHub

--- LeetCode/Medium/0198-house-robber/0198-house-robber.py
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.py
@@ -1,8 +1,5 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 0:
-            return 0
-        
         if len(nums) <= 2:
             return max(nums)
         
