530cf733b9b7e2713de4842c1c113e8aae7978a0
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-05-21 20:50:59 +0900

    [level 2] Title: 프로세스, Time: 0.49 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/README.md
+++ 프로그래머스/2/42587. 프로세스/README.md
@@ -1,10 +1,10 @@
 # [level 2] 프로세스 - 42587 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=python) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42587?language=python3) 
 
 ### 성능 요약
 
-메모리: 7.81 MB, 시간: 0.41 ms
+메모리: 10.1 MB, 시간: 0.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 21일 20:49:40
+2024년 05월 21일 20:50:55
 
 ### 문제 설명
 
--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,22 +1,23 @@
-from collections import deque
+from collections import deque;
 
 def solution(priorities, location):
-    # (우선순위, 인덱스) 형태의 튜플 리스트를 만듭니다.
-    queue = deque((priority, idx) for idx, priority in enumerate(priorities))
+    index_list = []
+    
+    for i in range(len(priorities)):
+        temp = [priorities[i], i]
+        index_list.append(temp)
+        
+    queue = deque(index_list)
     
     answer = 0
     while True:
-        # 큐의 첫 번째 요소를 꺼냅니다.
         front = queue.popleft()
-        # 큐에서 가장 높은 우선순위를 찾습니다.
-        if queue and max(queue, key=lambda x: x[0])[0] > front[0]:
-            # 현재 문서보다 높은 우선순위가 존재하면 다시 큐의 뒤에 넣습니다.
+        if queue and max(queue, key=lambda x:x[0])[0] > front[0]:
             queue.append(front)
+        elif front[1] == location:
+            break
         else:
-            # 그렇지 않으면 문서를 출력합니다.
             answer += 1
-            # 만약 출력한 문서가 우리가 찾는 문서라면 반복을 종료합니다.
-            if front[1] == location:
-                break
             
-    return answer
+            
+    return answer + 1
