10eabbb9a78a07e12441a59c9ccb6a9b32d4429c
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-05-23 03:11:12 +0900

    [level 1] Title: 체육복, Time: 0.00 ms, Memory: 7.89 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?language=python) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.01 ms
+메모리: 7.89 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 23일 03:08:43
+2024년 05월 23일 03:11:10
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -2,16 +2,16 @@ def solution(n, lost, reserve):
     new_lost = set(lost) - set(reserve)
     new_reserve = set(reserve) - set(lost)
     
-    to_remove = set()  # 제거할 요소를 저장할 집합
+    count = 0
     
     for i in new_lost:
         if (i - 1) in new_reserve:
-            to_remove.add(i)
             new_reserve.remove(i - 1)
+            count += 1
         elif (i + 1) in new_reserve:
-            to_remove.add(i)
             new_reserve.remove(i + 1)
+            count += 1
+            
+    answer = len(new_lost) - count
     
-    new_lost -= to_remove  # 반복문이 끝난 후 제거
-    
-    return n - len(new_lost)
+    return n - answer
