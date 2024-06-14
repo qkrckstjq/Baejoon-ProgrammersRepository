f9155d8c21915efa67bd5242337fedc9264e53ff
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-06-14 17:58:59 +0900

    [level 2] Title: 피로도, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,15 +1,18 @@
-def solution(k, dungeons):    
-    answer = -1
-    stack = [[0, 0, k, set()]]
-    while stack:
-        cur_i, count, cur_k, visit = stack.pop()
-        min_v = dungeons[cur_i][0]
-        use_v = dungeons[cur_i][1]
-        answer = max(count, answer)
+def solution(k, dungeons):
+    answer = 0
+
+    def dfs(health, dungeons, visited, count):
+        nonlocal answer
+        if count > answer:
+            answer = count
+
         for i in range(len(dungeons)):
-            if not i in visit and cur_k >= dungeons[i][0]:
-                new_visit = visit.copy()
-                new_visit.add(i)
-                stack.append([i, count + 1, cur_k - dungeons[i][1], new_visit])
-                
+            if not visited[i] and health >= dungeons[i][0]:
+                visited[i] = True
+                dfs(health - dungeons[i][1], dungeons, visited, count + 1)
+                visited[i] = False
+
+    visited = [False] * len(dungeons)
+    dfs(k, dungeons, visited, 0)
+
     return answer
