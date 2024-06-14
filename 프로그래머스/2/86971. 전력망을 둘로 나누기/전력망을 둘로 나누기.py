711bcbe8b0d90360a442563f5b34919bfb2cc046
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-06-14 18:40:31 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: ., Memory: undefined -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,21 +1,6 @@
-def dfs(graph, src):
-    stack = [src]
-    count = 0
-    visit = set()
-    while stack:
-        cur_src = stack.pop()
-        if not cur_src in visit:
-            visit.add(cur_src)
-            count += 1
-            for child in graph[cur_src]:
-                if not child in visit:
-                    stack.append(child)
-    return count
-
 def solution(n, wires):
     graph = {}
-    answer = n
-
+    
     for s, d in wires:
         if s in graph:
             graph[s].append(d)
@@ -25,17 +10,7 @@ def solution(n, wires):
             graph[d].append(s)
         else:
             graph[d] = [s]
+    print(graph)
     
-    for s, d in wires:
-        graph[s].remove(d)
-        graph[d].remove(s)
-        
-        parent = dfs(graph, 1)
-        child = n - parent
-
-        answer = min(answer, abs(parent - child))
-        
-        graph[s].append(d)
-        graph[d].append(s)
-    
+    answer = -1
     return answer
