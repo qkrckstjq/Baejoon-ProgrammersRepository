196f6d07f7ca8228751171a0bac3a4788475e16d
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-10-24 16:47:35 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 448 ms, Memory: 137704 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -1,20 +1,19 @@
 import heapq
 
-N, E = map(int, input().split(" "))
-graph = {i: {} for i in range(1, N+1)}  # 모든 노드 초기화        
+N, E = list(map(int, input().split(" ")))
+graph = {i: {} for i in range(1, N+1)}  # 모든 노드를 초기화        
 
 for _ in range(E):
-    src, dst, w = map(int, input().split(" "))
+    src, dst, w = list(map(int, input().split(" ")))
     graph[src][dst] = w
-    graph[dst][src] = w
+    graph[dst][src] = w  # 양방향 그래프
 
-target1, target2 = map(int, input().split(" "))
+target1, target2 = list(map(int, input().split(" ")))
 
-def dijkstra(src, dst):
+def dieck(src, dst):
     min_heap = [(0, src)]
-    distances = {i: float('inf') for i in range(1, N+1)}
-    distances[src] = 0
     visit = set()
+    dist = {src: 0}
     
     while min_heap:
         cur_w, cur_src = heapq.heappop(min_heap)
@@ -25,23 +24,18 @@ def dijkstra(src, dst):
         if cur_src == dst:
             return cur_w
         
-        for next_dst, next_w in graph[cur_src].items():
-            if next_dst not in visit:
-                new_w = cur_w + next_w
-                if new_w < distances[next_dst]:
-                    distances[next_dst] = new_w
-                    heapq.heappush(min_heap, (new_w, next_dst))
-    
-    return float('inf')  # 경로가 없을 경우 무한대 반환
+        for next_dst, w in graph[cur_src].items():
+            next_w = cur_w + w
+            if dist.get(next_dst, float('inf')) > next_w:
+                dist[next_dst] = next_w
+                heapq.heappush(min_heap, (next_w, next_dst))
 
-# 가능한 경로 계산
-case1 = dijkstra(1, target1) + dijkstra(target1, target2) + dijkstra(target2, N)
-case2 = dijkstra(1, target2) + dijkstra(target2, target1) + dijkstra(target1, N)
+    return float('inf')
 
-# 결과 확인
-result = min(case1, case2)
+case1 = dieck(1, target1) + dieck(target1, target2) + dieck(target2, N)
+case2 = dieck(1, target2) + dieck(target2, target1) + dieck(target1, N)
 
-if result == float('inf'):
+if case1 == float('inf') and case2 == float('inf'):
     print(-1)
 else:
-    print(result)
+    print(min(case1, case2))
