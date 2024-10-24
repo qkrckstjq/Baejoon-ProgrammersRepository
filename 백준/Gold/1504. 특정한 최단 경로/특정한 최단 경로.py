a00bc38a49cb432aa102693c5e5bf021c22aae93
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-10-24 16:48:22 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 400 ms, Memory: 137480 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.py
@@ -1,39 +1,42 @@
 import heapq
 
 N, E = list(map(int, input().split(" ")))
-graph = {i: {} for i in range(1, N+1)}  # 모든 노드를 초기화        
+graph = {i: {} for i in range(1, N+1)}      
 
 for _ in range(E):
     src, dst, w = list(map(int, input().split(" ")))
-    graph[src][dst] = w
-    graph[dst][src] = w  # 양방향 그래프
+    if src in graph:
+        graph[src][dst] = w
+    else:
+        graph[src] = {dst : w}
+    if dst in graph:
+        graph[dst][src] = w
+    else:
+        graph[dst] = {src : w}
 
 target1, target2 = list(map(int, input().split(" ")))
 
 def dieck(src, dst):
     min_heap = [(0, src)]
-    visit = set()
+    visit = set()    
     dist = {src: 0}
-    
     while min_heap:
         cur_w, cur_src = heapq.heappop(min_heap)
         if cur_src in visit:
             continue
         visit.add(cur_src)
-        
         if cur_src == dst:
             return cur_w
-        
         for next_dst, w in graph[cur_src].items():
             next_w = cur_w + w
             if dist.get(next_dst, float('inf')) > next_w:
                 dist[next_dst] = next_w
                 heapq.heappush(min_heap, (next_w, next_dst))
-
     return float('inf')
 
 case1 = dieck(1, target1) + dieck(target1, target2) + dieck(target2, N)
 case2 = dieck(1, target2) + dieck(target2, target1) + dieck(target1, N)
+
 
 if case1 == float('inf') and case2 == float('inf'):
     print(-1)
