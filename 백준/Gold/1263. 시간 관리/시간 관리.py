b482b333667f0bd69ff42637f6ca9e646304c9e5
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-10-31 16:47:30 +0900

    [Gold V] Title: 시간 관리, Time: 60 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/1263. 시간 관리/시간 관리.py
+++ 백준/Gold/1263. 시간 관리/시간 관리.py
@@ -1,19 +1,7 @@
-import heapq
-
-def latest_start_time(N, tasks):
-    tasks.sort(key=lambda x: x[1], reverse=True)
-
-    start_time = tasks[0][1]
-    
-    for t, s in tasks:
-        start_time = min(start_time, s)  
-        start_time -= t  
-        
-        if start_time < 0:
-            return -1
-
-    return start_time
-
 N = int(input())
-tasks = [tuple(map(int, input().split())) for _ in range(N)]
-print(latest_start_time(N, tasks))
+works_time = sorted([list(map(int, input().split(" "))) for _ in range(N)], key=lambda x : x[1], reverse=True)
+start_min = works_time[0][1]
+for t, s in works_time:
+    start_min = min((s - t), start_min - t)
+        
+print(-1 if start_min < 0 else start_min)
