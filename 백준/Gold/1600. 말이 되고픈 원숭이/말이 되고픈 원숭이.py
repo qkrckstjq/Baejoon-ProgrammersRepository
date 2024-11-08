6064a55cc06aa0276cf28d8e2809de94a46c5a3b
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-11-08 16:40:38 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 1268 ms, Memory: 222044 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
@@ -9,6 +9,15 @@ hy = [1, -1, 1, -1, 2, -2, -2, 2]
 dx = [0, 0, 1, -1]
 dy = [1, -1, 0, 0]
 
+def check_move(y, x):
+    return True if input_map[y][x] == 0 else False
+
+def check_range(y, x):
+    return True if 0 <= y < len(input_map) and 0 <= x < len(input_map[0]) else False
+
+def check_visit(y, x, k, visit):
+    return True if not visit[y][x][k] else False
+
 queue = deque()
 visit = [[[False] * (K + 1) for _ in range(W)] for _ in range(H)]
 queue.append((0, 0, K, 0))
@@ -22,19 +31,19 @@ while queue:
         find = True
         break
     
-    if not visit[cur_y][cur_x][cur_k]:
+    if not visit[cur_y][cur_x][cur_k]:  # 방문 여부 체크 후 표시
         visit[cur_y][cur_x][cur_k] = True
-        
+
         if cur_k > 0:
             next_k = cur_k - 1
             for i in range(8):
                 next_y, next_x = cur_y + hy[i], cur_x + hx[i]
-                if 0 <= next_y < H and 0 <= next_x < W and input_map[next_y][next_x] == 0 and not visit[next_y][next_x][next_k]:
+                if check_range(next_y, next_x) and check_move(next_y, next_x) and check_visit(next_y, next_x, next_k, visit):
                     queue.append((next_y, next_x, next_k, cur_move + 1))
-        
+
         for i in range(4):
             next_y, next_x = cur_y + dy[i], cur_x + dx[i]
-            if 0 <= next_y < H and 0 <= next_x < W and input_map[next_y][next_x] == 0 and not visit[next_y][next_x][cur_k]:
+            if check_range(next_y, next_x) and check_move(next_y, next_x) and check_visit(next_y, next_x, cur_k, visit):
                 queue.append((next_y, next_x, cur_k, cur_move + 1))
 
 if not find:
