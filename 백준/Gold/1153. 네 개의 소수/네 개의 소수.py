6ba93487a3d75f4ae060efd4e40ff9433d31f7fd
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-11-09 16:44:46 +0900

    [Gold III] Title: 네 개의 소수, Time: 132 ms, Memory: 133072 KB -BaekjoonHub

--- 백준/Gold/1153. 네 개의 소수/네 개의 소수.py
+++ 백준/Gold/1153. 네 개의 소수/네 개의 소수.py
@@ -1,31 +1,28 @@
-import sys
-import math
+N = int(input())
 
-def sieve(limit):
-    is_prime = [True] * (limit + 1)
-    is_prime[0], is_prime[1] = False, False
-    for i in range(2, int(math.sqrt(limit)) + 1):
-        if is_prime[i]:
-            for j in range(i * i, limit + 1, i):
-                is_prime[j] = False
-    return [num for num, prime in enumerate(is_prime) if prime]
+def get_prime_list(number):
+    prime = [True for _ in range(number + 1)]
+    prime[0], prime[1] = False, False
+    for i in range(2, int(number**0.5) + 1):
+        if prime[i]:
+            for j in range(i**2, number + 1, i):
+                prime[j] = False
+    return [i for i in range(2, len(prime)) if prime[i]]
 
-def find_four_primes_sum(N):
-    primes = sieve(N)
-    prime_set = set(primes)
-
-    for p1 in primes:
-        for p2 in primes:
-            for p3 in primes:
-                p4 = N - p1 - p2 - p3
-                if p4 in prime_set:
-                    return p1, p2, p3, p4
+def result(prime_list, target):
+    for n1 in prime_list:
+        for n2 in prime_list:
+            for n3 in prime_list:
+                n4 = target - n1 - n2 - n3
+                if n4 in prime_list:
+                    return [n1, n2, n3, n4]
     return -1
 
-N = int(input())
-result = find_four_primes_sum(N)
+prime_list = get_prime_list(N)
+prime = set(prime_list)
+answer = result(prime, N)
 
-if result == -1:
-    print(result)
+if answer == -1:
+    print(-1)
 else:
-    print(" ".join(map(str, result)))
+    print(" ".join(list(map(str, answer))))
