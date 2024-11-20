a4f0e353f80fc5a971745ab6cdbed0487462cee7
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-11-20 14:13:23 +0900

    [Gold V] Title: 벼락치기, Time: 96 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Gold/14728. 벼락치기/벼락치기.py
@@ -0,0 +1,18 @@
+def max_score_optimized(N, T, chapters):
+    # DP 배열 초기화
+    dp = [0] * (T + 1)
+    
+    # 각 단원에 대해 DP 갱신
+    for K, S in chapters:
+        for j in range(T, K - 1, -1):  # 뒤에서부터 순회
+            dp[j] = max(dp[j], dp[j - K] + S)
+    
+    return dp[T]
+
+N, T = list(map(int, input().split(" ")))
+chapters = []
+for _ in range(N):
+    time, value = list(map(int, input().split(" ")))
+    chapters.append([time, value])
+
+print(max_score_optimized(N, T, chapters))
