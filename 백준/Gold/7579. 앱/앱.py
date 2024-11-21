5336da9ab04d1142cbfa7e3278d9152266bbfed4
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-11-21 18:15:05 +0900

    [Gold III] Title: 앱, Time: 100 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.py
+++ 백준/Gold/7579. 앱/앱.py
@@ -1,17 +1,11 @@
-N, M = map(int, input().split(" "))
+N, Q = list(map(int, input().split(" ")))
 bytes = list(map(int, input().split(" ")))
 costs = list(map(int, input().split(" ")))
-
-# 비용의 최대 합계를 계산
-max_cost = sum(costs)
-dp = [0] * (max_cost + 1)  # 비용별로 확보할 수 있는 최대 바이트 수 기록
-
+M = sum(costs)
+dp = [0] * (M + 1)
 for i in range(N):
     byte = bytes[i]
     cost = costs[i]
-    for j in range(max_cost, cost - 1, -1):
+    for j in range(M, cost - 1, -1):
         dp[j] = max(dp[j], dp[j - cost] + byte)
-
-# 최소 비용을 찾음
-result = min(cost for cost, mem in enumerate(dp) if mem >= M)
-print(result)
+print(min([cost for cost in range(M + 1) if dp[cost] >= Q]))
