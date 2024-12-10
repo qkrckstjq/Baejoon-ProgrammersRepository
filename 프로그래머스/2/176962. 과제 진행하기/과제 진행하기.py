69eeab7951e773c69e13f4111aa40ee2db49df16
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-12-10 18:03:43 +0900

    [level 2] Title: 과제 진행하기, Time: 6.08 ms, Memory: 10.7 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.py
@@ -1,48 +1,51 @@
+def trans_time(time):
+    hour, minute = time.split(":")
+    return int(hour)*60 + int(minute)
+
 def solution(plans):
-    def trans_time(time):
-        hour, minute = time.split(":")
-        return int(hour) * 60 + int(minute)
-
-    # 플랜을 시작 시간 기준으로 정렬
-    plans.sort(key=lambda x: trans_time(x[1]))
-
+    plans.sort(key = lambda x : trans_time(x[1]))
+    # print(plans)
     stack = []
     answer = []
-
     for i in range(len(plans) - 1):
-        current_plan = plans[i]
+        plan = plans[i]
+        sub = plan[0]
+        start_time = trans_time(plan[1])
+        delay = int(plan[2])
+        
         next_plan = plans[i + 1]
-
-        current_subject, current_start, current_duration = current_plan
-        next_subject, next_start, _ = next_plan
-
-        current_start_time = trans_time(current_start)
-        next_start_time = trans_time(next_start)
-        duration = int(current_duration)
-
-        rest_time = next_start_time - (current_start_time + duration)
-
+        next_sub = next_plan[0]
+        next_start_time = trans_time(next_plan[1])
+        next_delay = int(next_plan[2])
+        
+        rest_time = next_start_time - (start_time + delay)
+        # print(rest_time)
         if rest_time >= 0:
-            # 현재 작업 완료 후 다음 작업 시작
-            answer.append(current_subject)
-            # 스택에 쌓여 있는 작업 처리
+            answer.append(sub)
+            # print(stack)
             while stack and rest_time > 0:
-                last_subject, remaining_time = stack.pop()
-                if rest_time >= remaining_time:
-                    answer.append(last_subject)
-                    rest_time -= remaining_time
+                last_plan_sub, last_plan_time = stack[-1][0], stack[-1][1]
+                if rest_time >= last_plan_time:
+                    stack.pop()
+                    answer.append(last_plan_sub)
+                    rest_time -= last_plan_time
                 else:
-                    stack.append((last_subject, remaining_time - rest_time))
+                    stack[-1][1] -= rest_time
                     rest_time = 0
         else:
-            # 다음 작업 시작 전 현재 작업 미완료
-            stack.append((current_subject, -rest_time))
-
-    # 마지막 작업 추가
-    answer.append(plans[-1][0])
-
-    # 스택에 남아 있는 작업 추가
+            stack.append([sub, -rest_time])
+            
+        if i == (len(plans) - 2):
+            answer.append(plans[i + 1][0])
     while stack:
         answer.append(stack.pop()[0])
-
+        
+    # print(answer)
+    # print(stack)
+        
+    
+    # while stack:
+    #     answer.append(stack.pop()[1])
+    
+    
     return answer
