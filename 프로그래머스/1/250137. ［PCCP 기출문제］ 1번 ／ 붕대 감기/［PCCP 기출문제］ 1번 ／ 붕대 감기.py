7878b385f4016fb3e9d984b567da6d8ab2c6d914
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2024-12-13 14:18:53 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 붕대 감기, Time: 0.04 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
+++ 프로그래머스/1/250137. ［PCCP 기출문제］ 1번 ／ 붕대 감기/［PCCP 기출문제］ 1번 ／ 붕대 감기.py
@@ -1,32 +1,12 @@
 def solution(bandage, health, attacks):
-    max_health = health
-    def add_health(health, num):
-        return health + num if health + num <= max_health else max_health
-    
-    def healing(health, time_dif):
-        raps = (time_diff - 1) // bandage[0]
-        per_heal = (time_diff - 1) % bandage[0]
-        health = add_health(health, (bandage[0] * bandage[1] + bandage[2]) * raps)
-        health = add_health(health, per_heal * bandage[1])
-        return health
-                            
-    for i in range(len(attacks) - 1):
-        time = attacks[i][0]
-        damage = attacks[i][1]
-        
-        next_time = attacks[i + 1][0]
-        # next_damage = attacks[i + 1][1]
-        
-        time_diff = next_time - time
-        health -= damage
-        # print("뎀지 받음", health)
-        if health < 1:
-            break
-        health = healing(health, time_diff)
-        # print("힐 받음", health)
-        
-    health -= attacks[-1][1]
-    if health < 1:
-        return -1
-    else:
-        return health
+    hp = health
+    start = 1
+    for i, j in attacks:
+        hp += ((i - start) // bandage[0]) * bandage[2] + (i - start) * bandage[1]
+        start = i + 1
+        if hp >= health:
+            hp = health
+        hp -= j
+        if hp <= 0:
+            return -1
+    return hp
