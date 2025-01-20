690d7c5f39923e2c6b9e104117a34ec24676782f
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-01-20 14:53:24 +0900

    [Bronze II] Title: Hashing, Time: 92 ms, Memory: 108384 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,9 +1,18 @@
-N = int(input())
-string = list(input())
+# 입력 처리
+L = int(input())  # 문자열 길이
+string = input().strip()  # 해시를 계산할 문자열
 
-answer = 0
+# 상수값
+r = 31
+M = 1234567891
 
-for i in range(len(string)):
-    answer += ((31**i) * (ord(string[i]) - 96))
+# 해시 계산
+hash_value = 0
+power = 1  # r^i 값을 저장
 
-print(answer)
+for i in range(L):
+    char_value = ord(string[i]) - 96  # 'a'를 1로 변환
+    hash_value = (hash_value + char_value * power) % M
+    power = (power * r) % M  # r^i 값을 갱신하며 모듈러 연산
+
+print(hash_value)
