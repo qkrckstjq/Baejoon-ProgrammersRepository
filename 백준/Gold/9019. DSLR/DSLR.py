3bf2d8c8f3380cc4ac018ba7a7228fe9b31b783f
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-01-21 14:56:29 +0900

    [Gold IV] Title: DSLR, Time: 8944 ms, Memory: 216200 KB -BaekjoonHub

--- 백준/Gold/9019. DSLR/DSLR.py
+++ 백준/Gold/9019. DSLR/DSLR.py
@@ -4,8 +4,8 @@ T = int(input())
 
 def bfs(start, target):
     queue = deque([(start, "")])
-    visit = set()
-    visit.add(start)
+    visit = [False for _ in range(10000)]
+    visit[start] = True
     while queue:
         cur_num, cur_c = queue.popleft()
         if cur_num == target:
@@ -14,24 +14,24 @@ def bfs(start, target):
         
         next_d = cur_num * 2
         next_d = next_d % 10000 if next_d > 9999 else next_d
-        if next_d not in visit:
-            visit.add(next_d)
+        if not visit[next_d]:
+            visit[next_d] = True
             queue.append((next_d, f"{cur_c}D"))
             
         next_s = cur_num - 1
         next_s = 9999 if next_s < 0 else next_s
-        if next_s not in visit:
-            visit.add(next_s)
+        if not visit[next_s]:
+            visit[next_s] = True
             queue.append((next_s, f"{cur_c}S"))
         
         next_l = (cur_num % 1000) * 10 + cur_num // 1000
-        if next_l not in visit:
-            visit.add(next_l)
+        if not visit[next_l]:
+            visit[next_l] = True
             queue.append((next_l, f"{cur_c}L"))
         
         next_r = ((cur_num % 10) * 1000) + (cur_num // 10)
-        if next_r not in visit:
-            visit.add(next_r)
+        if not visit[next_r]:
+            visit[next_r] = True
             queue.append((next_r, f"{cur_c}R"))
             
 for _ in range(T):
