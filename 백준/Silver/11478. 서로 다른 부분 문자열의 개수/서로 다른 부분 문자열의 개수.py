7adc24df385278505be90f51645b4227f48d92b4
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-01-29 19:39:19 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 504 ms, Memory: 316700 KB -BaekjoonHub

---
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.py
@@ -0,0 +1,8 @@
+words = input()
+hash_map = set()
+for i in range(len(words)):
+    cur_w = ""
+    for j in range(i, len(words)):
+        cur_w += words[j]
+        hash_map.add(cur_w)
+print(len(hash_map))
