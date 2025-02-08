bfb5c037a8107950767cffa344c4c7d31bc04a53
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-02-08 18:26:39 +0900

    [Gold III] Title: 세 용액, Time: 152 ms, Memory: 111004 KB -BaekjoonHub

---
+++ 백준/Gold/2473. 세 용액/세 용액.py
@@ -0,0 +1,21 @@
+N = int(input())
+numbers = list(map(int, input().split(" ")))
+numbers.sort()
+answer = [1000000000, 1000000000, 1000000000]
+for i in range(N - 2):
+    j = i + 1
+    k = N - 1
+    if sum(answer) == 0:
+        break
+    while j < k:
+        sum_num = numbers[i] + numbers[j] + numbers[k]
+        sum_ans = answer[0] + answer[1] + answer[2]
+        if abs(sum_num) < abs(sum_ans):
+            answer = [numbers[i], numbers[j], numbers[k]]
+            
+        if sum_num < 0:
+            j += 1
+        else:
+            k -= 1
+
+print(" ".join(list(map(str, answer))))
