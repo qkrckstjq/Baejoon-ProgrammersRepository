8e8356f00f1854040ac5c50332458de29a4c01f7
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-02-10 16:36:38 +0900

    [Gold I] Title: 최종 순위, Time: 248 ms, Memory: 126764 KB -BaekjoonHub

---
+++ 백준/Gold/3665. 최종 순위/최종 순위.py
@@ -0,0 +1,57 @@
+from collections import deque
+T = int(input())
+
+for _ in range(T):
+    n = int(input())
+    prev_ranks = list(map(int, input().split(" ")))
+    m = int(input())
+    changes = []
+    graph = {i : [] for i in range(1, n + 1)}
+    indegree = [0 for _ in range(n + 1)]
+    for _ in range(m):
+        a, b = map(int, input().split(" "))
+        changes.append((a, b))
+    for i in range(n):
+        indegree[prev_ranks[i]] = i
+        for j in range(i + 1, n):
+            graph[prev_ranks[i]].append(prev_ranks[j])
+    
+    for b, a in changes:
+        if b in graph[a]:
+            graph[a].remove(b)
+            graph[b].append(a)
+            indegree[a] += 1
+            indegree[b] -= 1
+        else:
+            graph[b].remove(a)
+            graph[a].append(b)
+            indegree[a] -= 1
+            indegree[b] += 1
+    
+    queue = deque()
+    for i in range(1, n + 1):
+        if indegree[i] == 0:
+            queue.append(i)
+    answer = []
+    
+    find = 0
+    for i in range(n):
+        if len(queue) == 0:
+            find = 1
+            break
+        elif len(queue) > 1:
+            find = 2
+            break
+        cur_node = queue.popleft()
+        answer.append(cur_node)
+        for next_node in graph[cur_node]:
+            indegree[next_node] -= 1
+            if indegree[next_node] < 1:
+                queue.append(next_node)
+    
+    if find == 0:
+        print(" ".join(list(map(str, answer))))
+    elif find == 2:
+        print("?")
+    else:
+        print("IMPOSSIBLE")
