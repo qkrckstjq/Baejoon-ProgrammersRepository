9ad2ddf9575727acb288ddd3ff93b02afa5203dc
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-02-15 14:27:09 +0900

    [Gold IV] Title: 배수 공사, Time: 2008 ms, Memory: 153172 KB -BaekjoonHub

---
+++ 백준/Gold/15817. 배수 공사/배수 공사.py
@@ -0,0 +1,15 @@
+N, X = map(int, input().split(" "))
+pipes = []
+dp = [0 for _ in range(X + 1)]
+dp[0] = 1
+for _ in range(N):
+    L, C = map(int, input().split(" "))
+    pipes.append((L, C))
+
+for L, C in pipes:
+    for i in range(X, -1, -1):
+        for j in range(1, C + 1):
+            if i - L * j >= 0:
+                dp[i] += dp[i - L * j]
+
+print(dp[-1])
