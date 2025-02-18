aba95c1c4ebe5f1efa273b906fa7497d6a87dc33
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-02-18 20:36:40 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 148 ms, Memory: 126572 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -2,7 +2,7 @@ from collections import deque
 N, K = map(int, input().split(" "))
 visit = set()
 queue = deque([(0, N)])
-max_num = 1000000
+max_num = 100000
 while queue:
     cur_k, cur_x = queue.popleft()
     visit.add(cur_x)
@@ -12,12 +12,11 @@ while queue:
     next_1 = cur_x - 1
     next_2 = cur_x + 1
     next_3 = cur_x * 2
-    if next_3 not in visit and -max_num <= next_3 <= max_num:
+    if next_3 not in visit and 0 <= next_3 <= max_num:
         queue.appendleft((cur_k, next_3))
     
-    if next_1 not in visit and -max_num <= next_1 <= max_num:
+    if next_1 not in visit and 0 <= next_1 <= max_num:
         queue.append((cur_k + 1, next_1))
     
-    if next_2 not in visit and -max_num <= next_2 <= max_num:
+    if next_2 not in visit and 0 <= next_2 <= max_num:
         queue.append((cur_k + 1, next_2))
-        
