684465bab22e61c2d46e96d36d0ae214a8b142dd
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-02-18 19:37:36 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 328 ms, Memory: 202972 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -13,7 +13,7 @@ while queue:
     next_2 = cur_x + 1
     next_3 = cur_x * 2
     if next_3 not in visit and -max_num <= next_3 <= max_num:
-        queue.append((cur_k, next_3))
+        queue.appendleft((cur_k, next_3))
     
     if next_1 not in visit and -max_num <= next_1 <= max_num:
         queue.append((cur_k + 1, next_1))
