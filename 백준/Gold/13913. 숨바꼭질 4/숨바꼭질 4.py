1c034e92774c5252383f048144c511a78783dd1c
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-02-18 23:14:22 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 4332 ms, Memory: 241952 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -2,11 +2,9 @@ from collections import deque
 N, K = map(int, input().split(" "))
 max_num = 100000
 queue = deque([(0, N, f"{N}")])
-visit = set()
+visit = set([N])
 while queue:
     cur_t, cur_x, cur_route = queue.popleft()
-    
-    visit.add(cur_x)
     
     if cur_x == K:
         print(cur_t)
@@ -20,9 +18,12 @@ while queue:
     
     if next_3 not in visit and 0 <= next_3 <= max_num:
         queue.append((next_t, next_3, cur_route + " " + str(next_3)))
+        visit.add(next_3)
     
     if next_1 not in visit and 0 <= next_1 <= max_num:
         queue.append((next_t, next_1, cur_route + " " + str(next_1)))
+        visit.add(next_1)
         
     if next_2 not in visit and 0 <= next_2 <= max_num:
         queue.append((next_t, next_2, cur_route + " " + str(next_2)))
+        visit.add(next_2)
