f75191128668e1772e64a594d1d29ca2772e977d
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-04-11 16:53:14 +0900

    [Gold III] Title: 집으로, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1069. 집으로/집으로.py
+++ 백준/Gold/1069. 집으로/집으로.py
@@ -1,24 +1,15 @@
 X, Y, D, T = map(int, input().split(" "))
 
-dist = (X**2 + Y**2)**0.5
+dist = (X**2 + Y**2) ** 0.5
+jump = dist // D
 
-# jump = dist 방향으로 점프할 수 있는 최대 경우
-jump = dist//D
+case1 = dist
 
 if dist >= D:
-    case1 = T * jump + (dist - (D * jump))
-    case2 = dist
-    case3 = T * (jump + 1)
-    
-    result = min(case1, min(case2, case3))
-    result = round(result,9)
-    print(result)
-
+    case2 = jump * T + (dist - jump * D)
+    case3 = (jump + 1) * T
+    print(min(case1, min(case2, case3)))
 else:
-    case1 = T + (D - dist)
-    case2 = dist
+    case2 = T + (D - dist)
     case3 = T * 2
-    
-    result = min(case1, min(case2, case3))
-    result = round(result,9)
-    print(result)
+    print(min(case1, min(case2, case3)))
