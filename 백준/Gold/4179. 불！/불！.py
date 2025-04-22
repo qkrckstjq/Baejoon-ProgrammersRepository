65d0272808328fc6674e3df62954da85c0c09aea
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-04-22 15:42:26 +0900

    [Gold III] Title: 불!, Time: 1464 ms, Memory: 57624 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.py
+++ 백준/Gold/4179. 불！/불！.py
@@ -1,72 +1,63 @@
 from collections import deque
-import sys
-input = sys.stdin.readline
 
-R, C = map(int, input().split())
-board = [list(input().rstrip()) for _ in range(R)]
+R, C = map(int, input().split(" "))
+board = [list(input()) for _ in range(R)]
 
 dy = [1, -1, 0, 0]
 dx = [0, 0, 1, -1]
 
-# 1) 시작점과 초기 불 프론티어 준비
-s_y = s_x = 0
-fire_stack = deque()
-fire_visit = [[False]*C for _ in range(R)]
-
+s_y, s_x = 0, 0
+fire_queue = deque()
+fire_visit = [[False for _ in range(C)] for _ in range(R)]
+s_find, f_find = False, False
 for y in range(R):
     for x in range(C):
-        if board[y][x] == 'J':
+        if board[y][x] == "J":
             s_y, s_x = y, x
-            board[y][x] = '.'           # 출발 지점은 빈 칸으로
-        elif board[y][x] == 'F':
-            fire_stack.append((y, x))
-            fire_visit[y][x] = True     # 초기 불 위치 방문 표시
-
-def fire():
-    # 현재 프론티어 크기만큼만 처리
-    for _ in range(len(fire_stack)):
-        y, x = fire_stack.popleft()
-        for i in range(4):
-            ny, nx = y + dy[i], x + dx[i]
-            if 0 <= ny < R and 0 <= nx < C:
-                if not fire_visit[ny][nx] and board[ny][nx] == '.':
-                    fire_visit[ny][nx] = True
-                    board[ny][nx] = 'F'
-                    fire_stack.append((ny, nx))
-
-def print_board():
-    for row in board:
-        print(''.join(row))
-
-# 2) 지훈이 BFS
-queue = deque()
-queue.append((s_y, s_x, 0, 0))  # y, x, k(탈출시간), cnt(타임스텝)
-visit = [[False]*C for _ in range(R)]
-visit[s_y][s_x] = True
-
-cnt = 0
-escaped = False
-
-while queue and not escaped:
-    y, x, k, t = queue.popleft()
-
-    # 한 타임(초)이 바뀔 때마다 불 전파
-    if t == cnt:
-        fire()
-        cnt += 1
-
-    for i in range(4):
-        ny, nx = y + dy[i], x + dx[i]
-        # 보드 밖으로 나가면 탈출
-        if not (0 <= ny < R and 0 <= nx < C):
-            print(k + 1)
-            escaped = True
+        if board[y][x] == "F":
+            fire_queue.append((y, x))
+            fire_visit[y][x] = True
+        
+        if s_find and f_find:
             break
 
-        # 다음 칸이 빈 칸(.)이어야만 이동
-        if not visit[ny][nx] and board[ny][nx] == '.':
-            visit[ny][nx] = True
-            queue.append((ny, nx, k + 1, t + 1))
+def fire():
+    for _ in range(len(fire_queue)):
+        y, x = fire_queue.popleft()
+        for i in range(4):
+            next_y = y + dy[i]
+            next_x = x + dx[i]
+            if 0 <= next_y < R and 0 <= next_x < C:
+                if not fire_visit[next_y][next_x] and board[next_y][next_x] != "#":
+                    fire_visit[next_y][next_x] = True
+                    fire_queue.append((next_y, next_x))
+                    board[next_y][next_x] = "F"
+        
+queue = deque()
+queue.append((s_y, s_x, 0, 0))
+cnt = 0
 
-if not escaped:
+visit = [[False for _ in range(C)] for _ in range(R)]
+visit[s_y][s_x] = True
+find = False
+while queue and not find:
+    cur_y, cur_x, cur_k, cur_cnt = queue.popleft()
+    
+    if cnt == cur_cnt:
+        fire()
+        cnt += 1
+    
+    for i in range(4):
+        next_y = cur_y + dy[i]
+        next_x = cur_x + dx[i]
+        if 0 <= next_y < R and 0 <= next_x < C:
+            if not visit[next_y][next_x] and board[next_y][next_x] == ".":
+                visit[next_y][next_x] = True
+                queue.append((next_y, next_x, cur_k + 1, cur_cnt + 1))
+        else:
+            print(cur_k + 1)
+            find = True
+            break
+        
+if not find:
     print("IMPOSSIBLE")
