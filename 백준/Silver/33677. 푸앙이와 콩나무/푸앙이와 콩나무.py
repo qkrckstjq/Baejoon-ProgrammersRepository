fc9604dcd100ae706110acc8bfda6f78c7d583fb
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-04-23 15:40:56 +0900

    [Silver I] Title: 푸앙이와 콩나무, Time: 1068 ms, Memory: 337372 KB -BaekjoonHub

--- 백준/Silver/33677. 푸앙이와 콩나무/푸앙이와 콩나무.py
+++ 백준/Silver/33677. 푸앙이와 콩나무/푸앙이와 콩나무.py
@@ -1,10 +1,9 @@
 from collections import deque
-import heapq
 
 N = int(input())
 
-queue = []
-heapq.heappush(queue, (0, 0, 0))
+queue = deque()
+queue.append((0, 0, 0))
 
 def growth(cur_h, num):
     if num == 1:
@@ -17,7 +16,7 @@ def growth(cur_h, num):
 visit = {0 : [0, 0]} #{높이 : [일수 : 물의 양]}
 
 while queue:
-    cur_h, cur_d, cur_w = heapq.heappop(queue)
+    cur_h, cur_d, cur_w = queue.popleft()
     
     if cur_h == N:
         continue
@@ -29,6 +28,6 @@ while queue:
         if next_h <= N:
             if (next_h not in visit) or (visit[next_h][0] >= next_d and visit[next_h][1] > next_w):
                 visit[next_h] = [next_d, next_w]
-                heapq.heappush(queue, (next_h, next_d, next_w))
+                queue.append((next_h, next_d, next_w))
 
 print(visit[N][0], visit[N][1])
