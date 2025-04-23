e88fc94c3cfbb0ae721ecd39c43ac18fd7cff513
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-04-23 15:15:25 +0900

    [Silver I] Title: 푸앙이와 콩나무, Time: 1068 ms, Memory: 337372 KB -BaekjoonHub

---
+++ 백준/Silver/33677. 푸앙이와 콩나무/푸앙이와 콩나무.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+N = int(input())
+
+queue = deque()
+queue.append((0, 0, 0))
+
+def growth(cur_h, num):
+    if num == 1:
+        return (cur_h + 1)
+    elif num == 3:
+        return (cur_h * 3)
+    elif num == 5:
+        return (cur_h ** 2)
+
+visit = {0 : [0, 0]} #{높이 : [일수 : 물의 양]}
+
+while queue:
+    cur_h, cur_d, cur_w = queue.popleft()
+    
+    if cur_h == N:
+        continue
+
+    for w in [1, 3, 5]:
+        next_h = growth(cur_h, w)
+        next_d = cur_d + 1
+        next_w = cur_w + w
+        if next_h <= N:
+            if (next_h not in visit) or (visit[next_h][0] >= next_d and visit[next_h][1] > next_w):
+                visit[next_h] = [next_d, next_w]
+                queue.append((next_h, next_d, next_w))
+
+print(visit[N][0], visit[N][1])
