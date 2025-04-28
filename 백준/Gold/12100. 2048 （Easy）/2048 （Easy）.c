d2a9e4b7289e68788c709015ce9922683b082734
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-04-28 17:12:44 +0900

    [Gold I] Title: 2048 (Easy), Time: 4 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.c
@@ -0,0 +1,7 @@
+#define T;for(i=N*N;i--;)
+#define J(w,y)O(t,b,w,y)&&s<(i=K(c-1,t))&&(s=i),
+typedef int*P,S[400],Z;N,z;O(d,s,w,y)P d,s;{Z
+o=y<0?y-N*y:0,i=o,f=0;P g,h,l;for(;o+w*N-i;i+=w){for(h=s+i,g=d+i,l=&z;s+i+y*N-h;h+=y)*h&&(*l-*h?*(l=g)=*h,g+=y:(*l+=*h,l=&z));for(;d+i+y*N-g;g+=y)*g=0;}T f|=d[i]^s[i];return
+f;}K(c,b)P b;{Z i,s;S
+t;T s<b[i]&&(s=b[i]);return c?J(N,1)J(N,-1)J(1,N)J(1,-N)s:s;}main(i){S
+t;scanf("%d",&N)T scanf("%d",t+i);printf("%d",K(5,t));}
