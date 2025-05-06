77e23223e274067ca115a7c9179d68ffeca49421
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-05-06 23:43:30 +0900

    [Bronze V] Title: A+B, Time: 100 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,10 +1,12 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println(a+b);
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = reader.readLine().split(" ");
+
+        System.out.println(Integer.parseInt(input[0]) + Integer.parseInt(input[1]));
     }
 }
