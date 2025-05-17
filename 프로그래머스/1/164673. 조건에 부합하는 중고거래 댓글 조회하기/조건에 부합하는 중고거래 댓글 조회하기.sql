312a8ba9444cce359fbaa1fd137d874f9ce30c35
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-05-17 19:35:54 +0900

    [level 1] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/1/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,7 +1,7 @@
 -- 코드를 입력하세요
-SELECT b.TITLE, b.BOARD_ID, r.REPLY_ID, r.WRITER_ID,r.CONTENTS,
-SUBSTRING(r.created_date,1,10) as CREATED_DATE from USED_GOODS_BOARD b
+SELECT b.TITLE, b.BOARD_ID, r.REPLY_ID, r.WRITER_ID, r.CONTENTS, CAST(r.CREATED_DATE AS CHAR) from USED_GOODS_BOARD b
 inner join USED_GOODS_REPLY r
 on b.BOARD_ID = r.BOARD_ID
-where b.created_date between '2022-10-01' and '2022-10-31'
-order by r.created_date, b.title
+WHERE YEAR(b.CREATED_DATE)  = 2022
+  AND MONTH(b.CREATED_DATE) = 10
+order by r.CREATED_DATE, b.TITLE
