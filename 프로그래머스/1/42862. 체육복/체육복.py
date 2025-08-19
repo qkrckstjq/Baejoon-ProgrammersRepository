0eef19b2c32f75b38bd2dfbd4850b04f91d0416c
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-08-19 14:26:03 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 7.78 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -2,7 +2,7 @@ def solution(n, lost, reserve):
     new_lost = set(lost) - set(reserve)
     new_reserve = set(reserve) - set(lost)
     
-    count = 0
+    count = 0  
     
     for i in new_lost:
         if (i - 1) in new_reserve:
@@ -12,6 +12,6 @@ def solution(n, lost, reserve):
             new_reserve.remove(i + 1)
             count += 1
             
-    answer = len(new_lost) - count
+    answer = len(new_lost) - count;
     
     return n - answer
