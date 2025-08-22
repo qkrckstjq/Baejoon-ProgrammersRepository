1bc8fd72ee5e5e7911572c4829f7a996804d099b
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-08-23 08:54:10 +0900

    [level 2] Title: 더 맵게, Time: 1495.13 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=java) 
 
 ### 성능 요약
 
-메모리: 69.6 MB, 시간: 1989.49 ms
+메모리: 124 MB, 시간: 1495.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 19일 14:01:14
+2025년 08월 23일 08:54:04
 
 ### 문제 설명
 
---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for(int s : scoville) {
+            queue.offer(s);
+        }
+        
+        while(queue.peek() < K) {
+            if(queue.size() < 2) {
+                return -1;
+            }
+            Integer first = queue.poll();
+            Integer second = queue.poll();
+            queue.offer(first + second * 2);
+            answer += 1;
+        }
+        
+        return answer;
+    }
+}
