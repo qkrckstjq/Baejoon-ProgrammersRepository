e3794e740e9d7a8f472b519c97ac1283a44133bd
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-09-07 15:23:39 +0900

    [level 0] Title: 더 크게 합치기, Time: 1.84 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int a, int b) {
+        int answer = Math.max(Integer.parseInt(Integer.toString(a) + Integer.toString(b)), Integer.parseInt(Integer.toString(b) + Integer.toString(a)));
+        
+        return answer;
+    }
+}
