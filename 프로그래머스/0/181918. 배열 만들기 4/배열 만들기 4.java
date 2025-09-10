f3220fdcd372d69859159b87e5e9bc3e7c28e1ef
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-09-10 16:07:17 +0900

    [level 0] Title: 배열 만들기 4, Time: 53.51 ms, Memory: 81.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> stk = new ArrayList<>();
+        int i = 0;
+        while(i < arr.length) {
+            if(stk.isEmpty() || stk.get(stk.size() - 1) < arr[i]) {
+                stk.add(arr[i++]);
+            } else if(stk.get(stk.size() - 1) >= arr[i]){
+                stk.remove(stk.size() - 1);
+            }
+        }
+        int[] answer = new int[stk.size()];
+        for(int j = 0; j < stk.size(); j++) {
+            answer[j] = stk.get(j);
+        }
+        return answer;
+    }
+}
