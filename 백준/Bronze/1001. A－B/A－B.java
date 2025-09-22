9835dbb2b90e954955ffae6492bea27a7a9d4c55
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-09-22 13:49:34 +0900

    [Bronze V] Title: A-B, Time: 108 ms, Memory: 14620 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -1,10 +1,12 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        System.out.println(a-b);
+import java.util.*;
+import java.io.*;
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        int[] input = Arrays.stream(reader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int answer = input[0] - input[1];
+        writer.write(Integer.toString(answer));
+        writer.flush();
     }
 }
