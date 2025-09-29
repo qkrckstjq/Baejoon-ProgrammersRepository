a384edec76dec28f35a220dca0d079c7335f468a
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-09-29 15:19:00 +0900

    [level 0] Title: 배열 만들기 6, Time: 63.21 ms, Memory: 148 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr) {
+        List<Integer> stk = new ArrayList<>();
+        int i = 0;
+        while(i < arr.length) {
+            if(stk.isEmpty()) {
+                stk.add(arr[i++]);
+            } else if (stk.get(stk.size() - 1) == arr[i]) {
+                stk.remove(stk.size() - 1);
+                i++;
+            } else {
+                stk.add(arr[i++]);
+            }
+        }
+        if(stk.isEmpty()) stk = List.of(-1);
+        return stk.stream().mapToInt(q -> q).toArray();
+    }
+}
