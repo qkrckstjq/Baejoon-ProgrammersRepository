c4cbcbbaed1248b7cf38d7311eae447c1099cbb7
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-09-29 17:21:57 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 5.00 ms, Memory: 92.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.util.stream.*;
+class Solution {
+    public int[] solution(int[] num_list) {
+        Arrays.sort(num_list);
+        return IntStream.range(5, num_list.length).map(i -> num_list[i]).toArray();
+    }
+}
