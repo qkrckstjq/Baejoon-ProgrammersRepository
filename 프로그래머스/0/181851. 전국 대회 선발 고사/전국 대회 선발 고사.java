d7ca6dff6a82161f4dd9140088ca1c4b7f0d2662
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-09-30 15:11:42 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 10.20 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -4,14 +4,15 @@ class Solution {
     public int solution(int[] rank, boolean[] attendance) {
         return IntStream.range(0, rank.length)
             .filter(i -> attendance[i])
-            .boxed()                                                
-            .sorted(Comparator.comparingInt(i -> rank[i]))
+            .boxed()
+            .sorted(Comparator.comparing(i -> rank[i]))
             .limit(3)
-            .reduce(new int[]{0, 0}, (state, i) -> {
-                    int idx = state[0];
-                    int val = idx == 0 ? 10000 * i : idx == 1 ? 100 * i : i;
-                    return new int[]{idx + 1, state[1] + val};
-                },
-                (a, b) -> new int[]{0, a[1] + b[1]})[1];
+            .reduce(new int[]{0, 0},
+                   (total, i) -> {
+                       int idx = total[0];
+                       int val = idx == 0 ? 10000 * i : idx == 1 ? 100 * i : i;
+                       return new int[]{idx + 1, total[1] + val};
+                   },
+                   (a, b) -> new int[]{0, a[1] + b[1]})[1];
     }
 }
