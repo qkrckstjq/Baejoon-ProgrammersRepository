b226a6bb7dd71376f27057e8fd322a2aef7bf866
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-10-01 22:45:06 +0900

    [level 0] Title: 날짜 비교하기, Time: 1.03 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -0,0 +1,6 @@
+import java.time.*;
+class Solution {
+    public int solution(int[] date1, int[] date2) {
+        return LocalDate.of(date1[0], date1[1], date1[2]).isBefore(LocalDate.of(date2[0], date2[1], date2[2])) ? 1 : 0;
+    }
+}
