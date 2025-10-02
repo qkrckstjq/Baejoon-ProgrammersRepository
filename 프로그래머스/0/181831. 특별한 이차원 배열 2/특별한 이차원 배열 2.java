143180e7ab1d2ad15e994fa25704069206c8c6c9
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-10-02 12:08:52 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 13.38 ms, Memory: 91.4 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
@@ -2,12 +2,6 @@ import java.util.*;
 import java.util.stream.*;
 class Solution {
     public int solution(int[][] arr) {
-        int answer = 1;
-        for(int i = 0; i < arr.length; i++) {
-            for(int j = 0; j < arr.length; j++) {
-                if(arr[i][j] != arr[j][i]) answer = 0;
-            }
-        }
-        return answer;
+        return IntStream.range(0, arr.length).allMatch(i -> IntStream.range(0, arr.length).allMatch(j -> arr[i][j] == arr[j][i])) ? 1 : 0;
     }
 }
