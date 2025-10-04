dcd118468aedf9fc3c72a06f8db512a24b05a962
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-10-04 16:51:32 +0900

    [level 0] Title: 연속된 수의 합, Time: 2.88 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.java
+++ 프로그래머스/0/120923. 연속된 수의 합/연속된 수의 합.java
@@ -1,8 +1,6 @@
 import java.util.stream.*;
 class Solution {
     public int[] solution(int num, int total) {
-        return IntStream.range(0, num)
-            .map(i -> (total / num - (num - 1) / 2) + i)
-            .toArray();
+        return IntStream.range(0, num).map(i -> (total / num - (num - 1) / 2) + i).toArray();
     }
 }
