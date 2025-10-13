7b924f4eea3d5da319e16848ba9d5e693775f812
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-10-13 14:36:10 +0900

    [level 0] Title: 암호 해독, Time: 4.25 ms, Memory: 89.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.java
@@ -0,0 +1,10 @@
+import java.util.stream.*;
+import java.util.*;
+class Solution {
+    public String solution(String cipher, int code) {
+        return IntStream.range(0, cipher.length())
+            .filter(i -> (i + 1) % code == 0)
+            .mapToObj(i -> String.valueOf(cipher.charAt(i)))
+            .collect(Collectors.joining());
+    }
+}
