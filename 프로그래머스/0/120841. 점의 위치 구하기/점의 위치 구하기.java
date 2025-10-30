99c88bd14259dec997f30e37fdb2e9876e44271d
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-10-30 14:31:05 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.03 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int[] dot) {
+        return dot[0] > 0 && dot[1] > 0 ? 1 
+            : dot[0] > 0 && dot[1] < 0 ? 4
+            : dot[0] < 0 && dot[1] > 0 ? 2
+            : 3;
+    }
+}
