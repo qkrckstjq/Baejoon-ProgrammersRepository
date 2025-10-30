940db569977a194ac440c8059b99b6268fe828e7
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-10-30 14:22:42 +0900

    [level 0] Title: 공 던지기, Time: 0.07 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] numbers, int k) {
+        int answer = 0;
+        for(int i = 0; i < k - 1; i++) {
+            answer = (answer + 2 > numbers.length ? answer + 2 - numbers.length : answer + 2);
+        }
+        return answer + 1;
+    }
+}
