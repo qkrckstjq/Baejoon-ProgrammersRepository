b6c25dbea7258345b5185f174244c64d24356e60
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-11-25 17:04:14 +0900

    [level 1] Title: 실패율, Time: 7522.36 ms, Memory: 87.7 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,32 +1,21 @@
 import java.util.*;
 import java.util.stream.*;
+
 class Solution {
     public int[] solution(int N, int[] stages) {
-        Arrays.sort(stages);
-        double[] result = new double[N + 1];
-        double prev = 0;
-        for(int i = 0; i < stages.length; i++) {
-            if(stages[i] > N) break;
-            int cur_s = stages[i];
-            double user = 0;
-            double total = stages.length;
-            while(i < stages.length && cur_s == stages[i]) {
-                i++;
-                user++;
-            }
-            i--;
-            // System.out.printf("%d 스테이지 도달한 사람 %f, 실패한 사람 %f\n", cur_s, total - prev, user);
-            result[cur_s] = user / (total - prev);
-            prev += user;
-        }
-        // for(double r : result) System.out.println(r);
-        return IntStream.range(1, N + 1)
-        .boxed()
-        .sorted(
-            Comparator.comparingDouble((Integer i) -> result[i] * -1)
-                      .thenComparingInt(i -> i)                 
-        )
-        .mapToInt(Integer::intValue)
-        .toArray();
+        return IntStream.rangeClosed(1, N)
+            .boxed()
+            .sorted(
+                Comparator
+                    .comparingDouble((Integer stage) -> {
+                        long reach = Arrays.stream(stages).filter(s -> s >= stage).count();
+                        return reach == 0 ? 0.0 :
+                            (double) Arrays.stream(stages).filter(s -> s == stage).count() / reach;
+                    })
+                    .reversed()
+                    .thenComparingInt(i -> i)
+            )
+            .mapToInt(Integer::intValue)
+            .toArray();
     }
 }
