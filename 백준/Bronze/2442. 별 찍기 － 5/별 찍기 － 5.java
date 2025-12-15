b53725e84731a3c9b0300b8db14bf1af0ffd1777
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-12-15 16:16:42 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -2,6 +2,7 @@ import java.io.*;
 class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(rd.readLine());
         StringBuilder answer = new StringBuilder("");
         
@@ -11,6 +12,7 @@ class Main {
             answer.append("\n");
         }
         
-        System.out.println(answer);
+        wr.write(answer.toString());
+        wr.flush();
     }
 }
