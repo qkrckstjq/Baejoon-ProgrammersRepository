cf5298bd8e9a66e7e749ed6e4a45c24911422346
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2025-12-27 13:36:50 +0900

    [Bronze II] Title: 숫자 놀이, Time: 96 ms, Memory: 14012 KB -BaekjoonHub

--- 백준/Bronze/2145. 숫자 놀이/숫자 놀이.java
+++ 백준/Bronze/2145. 숫자 놀이/숫자 놀이.java
@@ -1,23 +1,29 @@
 import java.io.*;
 
 class Main {
+    public static int temp;
+    public static String result;
+    
     public static void main(String[] args) throws Exception {
         BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
         String input;
         while((input = rd.readLine()) != null && !input.equals("0")) {
-            String result = recursion(input);
+            result = "";
+            recursion(input);
             System.out.println(result);
         }
     }
     
-    public static String recursion(String sNum) {
+    public static void recursion(String sNum) {
         if(sNum.length() == 1) {
-            return sNum;
+            result = sNum;
+            return;
         }
-        int temp = 0;
+        temp = 0;
         for(int i = 0; i < sNum.length(); i++) {
             temp += sNum.charAt(i) - '0';
         }
-        return recursion(Integer.toString(temp));
+        recursion(Integer.toString(temp));
+        return;
     }
 }
