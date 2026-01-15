08d9de70d23fc293fb5ca880a43b42a99f084685
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2026-01-15 14:36:12 +0900

    [Bronze V] Title: Rook, Time: 104 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        StringBuilder answer = new StringBuilder("");
+        answer.append("  ___  ___  ___\n")
+            .append("  | |__| |__| |\n")
+            .append("  |           |\n")
+            .append("   \\_________/\n")
+            .append("    \\_______/\n")
+            .append("     |     |\n")
+            .append("     |     |\n")
+            .append("     |     |\n")
+            .append("     |     |\n")
+            .append("     |_____|\n")
+            .append("  __/       \\__\n")
+            .append(" /             \\\n")
+            .append("/_______________\\\n");
+        System.out.println(answer.toString());
+                    
+    }
+}
