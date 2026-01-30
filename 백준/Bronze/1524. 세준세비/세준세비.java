e2eac509ced3e95c77a7afd6f01bb66b7d8b2d91
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2026-01-30 15:17:01 +0900

    [Bronze I] Title: 세준세비, Time: 464 ms, Memory: 76784 KB -BaekjoonHub

--- 백준/Bronze/1524. 세준세비/세준세비.java
+++ 백준/Bronze/1524. 세준세비/세준세비.java
@@ -9,29 +9,16 @@ class Main {
         while(0 < T--) {
             String temp = rd.readLine();
             String[] sb = rd.readLine().split(" ");
-            List<Integer> s = Arrays.stream(rd.readLine().split(" "))
+            int s = Arrays.stream(rd.readLine().split(" "))
                 .map(Integer::parseInt)
-                .sorted()
-                .collect(Collectors.toList());
-            List<Integer> b = Arrays.stream(rd.readLine().split(" "))
+                .reduce(0, (result, n) -> Math.max(result, n));
+            int b = Arrays.stream(rd.readLine().split(" "))
                 .map(Integer::parseInt)
-                .sorted()
-                .collect(Collectors.toList());
-            
-            while(!s.isEmpty() && !b.isEmpty()) {
-                int lastS = s.get(s.size() - 1);
-                int lastB = b.get(b.size() - 1);
-                
-                if(lastS >= lastB) {
-                    b.remove(b.size() - 1);
-                } else {
-                    s.remove(s.size() - 1);
-                }
-            }
-            if(s.isEmpty()) {
-                System.out.println("B");
-            } else {
+                .reduce(0, (result, n) -> Math.max(result, n));
+            if(s >= b) {
                 System.out.println("S");
+            } else {
+                System.out.println("B");
             }
         }
     }
