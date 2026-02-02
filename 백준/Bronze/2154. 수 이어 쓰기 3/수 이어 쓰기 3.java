c608329ae9fa9fec859950e9f1991797f5830f36
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2026-02-02 15:50:00 +0900

    [Bronze II] Title: 수 이어 쓰기 3, Time: 132 ms, Memory: 23112 KB -BaekjoonHub

--- 백준/Bronze/2154. 수 이어 쓰기 3/수 이어 쓰기 3.java
+++ 백준/Bronze/2154. 수 이어 쓰기 3/수 이어 쓰기 3.java
@@ -1,14 +1,41 @@
 import java.io.*;
 
 public class Main {
+    static int[] prefix(char[] p) {
+        int m = p.length;
+        int[] pi = new int[m];
+        for (int i = 1, j = 0; i < m; i++) {
+            while (j > 0 && p[i] != p[j]) j = pi[j - 1];
+            if (p[i] == p[j]) pi[i] = ++j;
+        }
+        return pi;
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine().trim());
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= n; i++) sb.append(i);
+        char[] p = String.valueOf(n).toCharArray();
+        int m = p.length;
+        int[] pi = prefix(p);
 
-        String target = String.valueOf(n);
-        System.out.println(sb.indexOf(target) + 1);
+        int j = 0;
+        int pos = 0;
+
+        for (int x = 1; x <= n; x++) {
+            char[] s = String.valueOf(x).toCharArray();
+            for (int k = 0; k < s.length; k++) {
+                char c = s[k];
+                pos++;
+
+                while (j > 0 && c != p[j]) j = pi[j - 1];
+                if (c == p[j]) j++;
+
+                if (j == m) {
+                    System.out.println(pos - m + 1);
+                    return;
+                }
+            }
+        }
     }
 }
