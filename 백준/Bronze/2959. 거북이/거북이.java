9c9a537c4826779ad705c979bdadefbb7262c6da
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2026-02-04 14:55:00 +0900

    [Bronze III] Title: 거북이, Time: 108 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Bronze/2959. 거북이/거북이.java
+++ 백준/Bronze/2959. 거북이/거북이.java
@@ -4,10 +4,10 @@ import java.util.*;
 class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
-        int[] input = Arrays.stream(rd.readLine().split(" "))
-            .mapToInt(Integer::parseInt)
-            .sorted()
-            .toArray();
+        Integer[] input = Arrays.stream(rd.readLine().split(" "))
+            .map(Integer::parseInt)
+            .sorted(Comparator.comparing(i -> i))
+            .toArray(Integer[]::new);
         System.out.println(input[0] * input[2]);
     }
 }
