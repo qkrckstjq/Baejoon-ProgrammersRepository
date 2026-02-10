71108390ef252d530baad9f745090fd767bce421
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2026-02-10 15:51:01 +0900

    [Bronze III] Title: 캥거루 세마리, Time: 104 ms, Memory: 14204 KB -BaekjoonHub

---
+++ 백준/Bronze/2965. 캥거루 세마리/캥거루 세마리.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int left = b - a;
+        int right = c - b;
+
+        System.out.println(Math.max(left, right) - 1);
+    }
+}
