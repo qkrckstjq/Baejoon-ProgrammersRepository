cb886beac99209109e29aa274cba6ae9ecea55da
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2026-08-03 15:25:08 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.01 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.py
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.py
@@ -1,4 +1,12 @@
-from math import gcd
-
 def solution(w,h):
     return (w * h) - ((w + h) - gcd(w, h))
+
+def gcd(num1, num2):
+    a = max(num1, num2)
+    b = min(num1, num2)
+    
+    while a % b != 0:
+        temp = b
+        b = a % b
+        a = temp
+    return b
