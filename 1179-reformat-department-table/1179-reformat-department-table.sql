7ffd8dbda8936e4d4e5f527972b56a4c59561b5e
박찬섭 <117289923+qkrckstjq@users.noreply.github.com>
2026-09-15 18:02:46 +0900

    Time: 663 ms (19.85%), Space: 0B (100%) - LeetHub

---
+++ 1179-reformat-department-table/1179-reformat-department-table.sql
@@ -0,0 +1,20 @@
+/* Write your PL/SQL query statement below */
+SELECT *
+FROM Department d
+PIVOT (
+    SUM(revenue)
+    FOR month IN (
+        'Jan' AS Jan_Revenue,
+        'Feb' AS Feb_Revenue,
+        'Mar' AS Mar_Revenue,
+        'Apr' AS Apr_Revenue,
+        'May' AS May_Revenue,
+        'Jun' AS Jun_Revenue,
+        'Jul' AS Jul_Revenue,
+        'Aug' AS Aug_Revenue,
+        'Sep' AS Sep_Revenue,
+        'Oct' AS Oct_Revenue,
+        'Nov' AS Nov_Revenue,
+        'Dec' AS Dec_Revenue
+    )
+);
